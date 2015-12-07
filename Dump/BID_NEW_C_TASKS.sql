dc847d1c1daaa60c1fabaa812f7795e5527e9c76
Kenneth <pactoreal@msn.com>
2015-12-07 09:53:58 -0600

    Actualizacion de los scripts de BD

--- Dump/BID_NEW_C_TASKS.sql
+++ Dump/BID_NEW_C_TASKS.sql
@@ -30,7 +30,7 @@ CREATE TABLE `C_TASKS` (
   `HTTP_METHOD` varchar(10) DEFAULT 'GET',
   `CREATION_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID_TASK`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `C_TASKS` (
 
 LOCK TABLES `C_TASKS` WRITE;
 /*!40000 ALTER TABLE `C_TASKS` DISABLE KEYS */;
-INSERT INTO `C_TASKS` VALUES (1,'prueba/build-permissions','GET','2015-11-04 22:54:51'),(2,'prueba','GET','2015-11-13 22:50:54'),(4,'budget-categories','GET','2015-11-17 06:00:00'),(5,'budget-periods','GET','2015-11-18 00:51:28'),(6,'budget-types','GET','2015-11-18 00:51:28'),(8,'prueba/hashmap','GET','2015-11-18 23:27:36'),(10,'admon/budget','GET','2015-11-19 18:10:38'),(12,'request-types','GET','2015-11-20 17:01:29'),(13,'request-categories','GET','2015-11-20 17:01:53'),(14,'product-types','GET','2015-11-20 17:02:14'),(15,'branchs','GET','2015-11-20 23:46:18'),(16,'areas','GET','2015-11-20 23:46:18'),(17,'budgets','POST','2015-11-23 15:31:13'),(18,'budget-subcategories','GET','2015-11-23 23:21:16'),(19,'groups','GET','2015-11-25 19:00:20'),(20,'distributors','GET','2015-11-25 19:00:20'),(21,'request-types/#','GET','2015-11-27 16:12:27'),(22,'budgets/#/#/#/#','GET','2015-11-27 16:46:41'),(23,'prueba/#/#','GET','2015-11-27 17:59:34'),(24,'views','GET','2015-11-27 23:35:08'),(25,'views/#','GET','2015-11-27 23:35:25'),(26,'groups/#','GET','2015-11-28 00:16:04'),(27,'siad/budgets','GET','2015-11-30 15:54:39'),(28,'siad/pendign-requests','GET','2015-11-30 16:55:32'),(29,'siad/edit-request','GET','2015-11-30 17:03:20'),(30,'app-menu','GET','2015-12-01 16:21:33'),(31,'agenda','GET','2015-12-02 00:35:38');
+INSERT INTO `C_TASKS` VALUES (1,'prueba/build-permissions','GET','2015-11-04 22:54:51'),(2,'prueba','GET','2015-11-13 22:50:54'),(4,'budget-categories','GET','2015-11-17 06:00:00'),(5,'budget-periods','GET','2015-11-18 00:51:28'),(6,'budget-types','GET','2015-11-18 00:51:28'),(8,'prueba/hashmap','GET','2015-11-18 23:27:36'),(10,'admon/budget','GET','2015-11-19 18:10:38'),(12,'request-types','GET','2015-11-20 17:01:29'),(13,'request-categories','GET','2015-11-20 17:01:53'),(14,'product-types','GET','2015-11-20 17:02:14'),(15,'branchs','GET','2015-11-20 23:46:18'),(16,'areas','GET','2015-11-20 23:46:18'),(17,'budgets','POST','2015-11-23 15:31:13'),(18,'budget-subcategories','GET','2015-11-23 23:21:16'),(19,'groups','GET','2015-11-25 19:00:20'),(20,'distributors','GET','2015-11-25 19:00:20'),(21,'request-types/#','GET','2015-11-27 16:12:27'),(22,'budgets/#/#/#/#','GET','2015-11-27 16:46:41'),(23,'prueba/#/#','GET','2015-11-27 17:59:34'),(24,'views','GET','2015-11-27 23:35:08'),(25,'views/#','GET','2015-11-27 23:35:25'),(26,'groups/#','GET','2015-11-28 00:16:04'),(27,'siad/budgets','GET','2015-11-30 15:54:39'),(28,'siad/pendign-requests','GET','2015-11-30 16:55:32'),(29,'siad/edit-request','GET','2015-11-30 17:03:20'),(30,'app-menu','GET','2015-12-01 16:21:33'),(31,'agenda','GET','2015-12-02 00:35:38'),(32,'budgets/#/#','GET','2015-12-02 23:37:47'),(33,'budget-month-branch','GET','2015-12-03 18:54:55'),(34,'months','GET','2015-12-03 19:00:44'),(35,'dw-enterprises/#/#','GET','2015-12-03 23:17:30'),(36,'folios','GET','2015-12-04 18:45:23'),(37,'folios/authorizations','GET','2015-12-04 23:36:28');
 /*!40000 ALTER TABLE `C_TASKS` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-12-02 16:05:54
+-- Dump completed on 2015-12-04 18:45:57
