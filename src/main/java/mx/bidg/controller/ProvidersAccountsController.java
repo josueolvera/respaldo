e970a9a91327a56c88802d98eaaf892fd866658e
Kenneth <pactoreal@msn.com>
2015-12-22 11:49:44 -0600

    Logica para guardar ProviderAccount completa
    No testeada

--- src/main/java/mx/bidg/controller/ProvidersAccountsController.java
+++ src/main/java/mx/bidg/controller/ProvidersAccountsController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,6 +33,13 @@ public class ProvidersAccountsController {
     ProvidersAccountsService providersAccountsService;
     
     ObjectMapper mapper = new ObjectMapper();
+    
+    @RequestMapping(method = RequestMethod.POST, headers = {"Accept=application/json;charset=UTF-8"}, produces = "application/json;charset=UTF-8")
+    public @ResponseBody ResponseEntity<String> saveProviderAccount(@RequestBody String data) throws Exception {
+        ProvidersAccounts providersAccount = providersAccountsService.save(data);
+        return new ResponseEntity<>(mapper.writeValueAsString(providersAccount), HttpStatus.OK);
+        
+    }
     
     @RequestMapping(value = "/provider/{idProvider}", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     public @ResponseBody ResponseEntity<String> getByProvider(@PathVariable int idProvider) throws Exception {
--- src/main/java/mx/bidg/service/impl/ProvidersAccountsServiceImpl.java
+++ src/main/java/mx/bidg/service/impl/ProvidersAccountsServiceImpl.java
@@ -9,6 +9,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import mx.bidg.dao.ProvidersAccountsDao;
+import mx.bidg.model.Accounts;
+import mx.bidg.model.CAccountsTypes;
+import mx.bidg.model.CBanks;
 import mx.bidg.model.Providers;
 import mx.bidg.model.ProvidersAccounts;
 import mx.bidg.service.ProvidersAccountsService;
@@ -34,15 +37,30 @@ public class ProvidersAccountsServiceImpl implements ProvidersAccountsService {
     public ProvidersAccounts save(String data) throws Exception {
         
         JsonNode jsonRequest = mapper.readTree(data);
-        String provider = jsonRequest.get("provider").asText();
+        String providerName = jsonRequest.get("provider").asText();
         String rfc = jsonRequest.get("rfc").asText();
         Integer idBank = jsonRequest.get("bank").asInt();
-        String account = jsonRequest.get("account").asText();
+        String accountNumber = jsonRequest.get("account").asText();
         String clabe = jsonRequest.get("clabe").asText();
         
+        Providers provider = new Providers();
+        provider.setIdAccessLevel(1);
+        provider.setProviderName(providerName);
+        provider.setRfc(rfc);
         
+        Accounts account = new Accounts();
+        account.setAccountClabe(clabe);
+        account.setAccountNumber(accountNumber);
+        account.setIdAccessLevel(1);
+        account.setIdAccountType(new CAccountsTypes(1));
+        account.setIdBank(new CBanks(idBank));
         
-        return null;
+        ProvidersAccounts providersAccount = new ProvidersAccounts();
+        providersAccount.setIdAccessLevel(1);
+        providersAccount.setIdAccount(account);
+        providersAccount.setIdProvider(provider);
+        
+        return dao.save(providersAccount);
     }
     
 }
