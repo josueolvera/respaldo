ab5cccbb8a0deae7074ac0a05b2be089e12ea101
Rafael Viveros <rafaelvb25@gmail.com>
2015-12-30 17:53:24 -0600

    Modifica el bean multipartResolver para permitir documentos dinámicos
    Con el nuevo multipartResolver los documentos enviados al servidor
    se pueden obtener en una lista iterable

--- src/main/java/mx/bidg/config/AppConfig.java
+++ src/main/java/mx/bidg/config/AppConfig.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -50,12 +51,10 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         messageSource.setBasename("messages");
         return messageSource;
     }
-    
+
     @Bean(name = "multipartResolver")
-    public CommonsMultipartResolver createMultipartResolver() {
-        CommonsMultipartResolver resolver=new CommonsMultipartResolver();
-        resolver.setDefaultEncoding("utf-8");
-        return resolver;
+    public StandardServletMultipartResolver createMultipartResolver() {
+        return new StandardServletMultipartResolver();
     }
     
     @Override
--- src/main/java/mx/bidg/config/AppInitializer.java
+++ src/main/java/mx/bidg/config/AppInitializer.java
@@ -7,11 +7,19 @@ package mx.bidg.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+
 /**
  *
  * @author sistemask
  */
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    private static final String LOCATION = "/tmp/"; // Temporary location where files will be stored
+    private static final long MAX_FILE_SIZE = 5242880; // 5MB : Max file size.
+    private static final long MAX_REQUEST_SIZE = 22020096; // 21MB : Total request size containing Multi part.
+    private static final int FILE_SIZE_THRESHOLD = 0; // Size threshold after which files will be written to disk
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
@@ -27,5 +35,13 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
     protected String[] getServletMappings() {
         return new String[] {"/"};
     }
-    
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(getMultipartConfigElement());
+    }
+
+    private MultipartConfigElement getMultipartConfigElement() {
+        return new MultipartConfigElement(LOCATION, MAX_FILE_SIZE, MAX_REQUEST_SIZE, FILE_SIZE_THRESHOLD);
+    }
 }
