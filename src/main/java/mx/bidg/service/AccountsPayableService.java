416388dc6a029081adc0d41f75d07c937b8f94e3
Kenneth <pactoreal@msn.com>
2016-01-20 17:26:05 -0600

    Correcciones al obtener CBudgetConcepts por Budget

---
+++ src/main/java/mx/bidg/service/AccountsPayableService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.bidg.service;
+
+import java.util.List;
+import mx.bidg.model.AccountsPayable;
+
+/**
+ *
+ * @author sistemask
+ */
+public interface AccountsPayableService {
+    
+    public List<AccountsPayable> saveData(String data, int idRequest) throws Exception;
+    
+}
