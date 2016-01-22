f930665f69abbc35c3425a8b8d6d0309ade731c4
Rafael Viveros <rafaelvb25@gmail.com>
2016-01-21 18:37:52 -0600

    Agrega respuesta json al lanzar excepciones

--- src/main/java/mx/bidg/exceptions/ValidationException.java
+++ src/main/java/mx/bidg/exceptions/ValidationException.java
@@ -5,18 +5,58 @@
  */
 package mx.bidg.exceptions;
 
+import mx.bidg.pojos.ErrorField;
+import org.springframework.http.HttpStatus;
+
 /**
  *
  * @author sistemask
  */
 public class ValidationException extends RuntimeException {
-    
+
+    private HttpStatus status;
+    private String loggerMessage;
+    private String clientMessage;
+    private ErrorField errorField;
+
+    public ValidationException() {
+
+    }
+
     public ValidationException(String message) {
         super(message);
+        this.loggerMessage = message;
     }
-    
-    public ValidationException() {
-        
+
+    public ValidationException(String loggerMessage, String clientMessage) {
+        super(loggerMessage);
+        this.loggerMessage = loggerMessage;
+        this.clientMessage = clientMessage;
+        this.status = HttpStatus.BAD_REQUEST;
+        this.errorField = new ErrorField(clientMessage);
     }
-    
+
+    public ValidationException(String loggerMessage, String clientMessage, HttpStatus status) {
+        super(loggerMessage);
+        this.loggerMessage = loggerMessage;
+        this.clientMessage = clientMessage;
+        this.status = status;
+        this.errorField = new ErrorField(clientMessage);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getLoggerMessage() {
+        return loggerMessage;
+    }
+
+    public String getClientMessage() {
+        return clientMessage;
+    }
+
+    public ErrorField getError() {
+        return errorField;
+    }
 }
