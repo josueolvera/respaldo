754b1fbd2d43e1ee346ce64da356134e0a87f96f
Rafael Viveros <rafaelvb25@gmail.com>
2016-01-26 10:10:45 -0600

    Agrega método para manejar excepciones genéricas

--- src/main/java/mx/bidg/controller/GlobalControllerExceptionHandler.java
+++ src/main/java/mx/bidg/controller/GlobalControllerExceptionHandler.java
@@ -1,27 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mx.bidg.controller;
 
-import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import mx.bidg.exceptions.ValidationException;
-import org.springframework.core.annotation.AnnotationUtils;
+import mx.bidg.pojos.ErrorField;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -29,6 +20,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  */
 @ControllerAdvice
 public class GlobalControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> exceptionHandler(Exception e, WebRequest request) {
+        Logger.getLogger("GlobalControllerExceptionHandler").log(Level.SEVERE, e.getMessage(), e);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        ErrorField errors = new ErrorField(
+                null, "Ha habido un problema con su solicitud, intente nuevamente", HttpStatus.BAD_REQUEST.value()
+        );
+
+        return handleExceptionInternal(e, errors, headers, HttpStatus.BAD_REQUEST, request);
+    }
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<Object> validationExceptionHandler(Exception e, WebRequest request) {
