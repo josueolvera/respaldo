6fda386b2dfd986bcb5154cf687eb94ef431bc59
Josue Olvera <josue.olvera.r@gmail.com>
2016-04-14 13:17:11 -0500

    Agrega capas para CBanks

---
+++ src/main/java/mx/bidg/service/impl/CBanksServiceImp.java
@@ -0,0 +1,26 @@
+package mx.bidg.service.impl;
+
+import mx.bidg.dao.CBanksDao;
+import mx.bidg.model.CBanks;
+import mx.bidg.service.CBanksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by jolvera on 13/04/2016.
+ */
+@Service
+@Transactional
+public class CBanksServiceImp implements CBanksService {
+
+    @Autowired
+    private CBanksDao banksDao;
+
+    @Override
+    public List<CBanks> findAll() {
+        return banksDao.findAll();
+    }
+}
