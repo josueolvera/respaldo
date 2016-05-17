7c5442b19d6fe3f615236d56c748c08001120886
Rafael Viveros <rafaelvb25@gmail.com>
2016-05-17 18:27:01 -0500

    Agrega consulta de BudgetMonthConcepts por concepto

--- src/main/java/mx/bidg/controller/BudgetMonthConceptsController.java
+++ src/main/java/mx/bidg/controller/BudgetMonthConceptsController.java
@@ -7,20 +7,20 @@ package mx.bidg.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+
+import java.io.IOException;
 import java.util.List;
 import mx.bidg.config.JsonViews;
 import mx.bidg.model.BudgetMonthBranch;
 import mx.bidg.model.BudgetMonthConcepts;
+import mx.bidg.model.CBudgetConcepts;
 import mx.bidg.service.BudgetMonthConceptsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -31,9 +31,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class BudgetMonthConceptsController {
     
     @Autowired
-    BudgetMonthConceptsService budgetMonthConceptsService;
+    private BudgetMonthConceptsService budgetMonthConceptsService;
     
-    ObjectMapper map = new ObjectMapper().registerModule(new Hibernate4Module());
+    private ObjectMapper map = new ObjectMapper().registerModule(new Hibernate4Module());
+
+    @RequestMapping(value = "/{idConcept}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> findByConcept(@PathVariable int idConcept) throws IOException {
+        List<BudgetMonthConcepts> list = budgetMonthConceptsService.findByConcept(new CBudgetConcepts(idConcept));
+        return new ResponseEntity<>(
+                map.writerWithView(JsonViews.Embedded.class).writeValueAsString(list), HttpStatus.OK
+        );
+    }
     
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public @ResponseBody ResponseEntity<String> saveBudgetMonthConceptsList(@RequestBody String data) throws Exception {
