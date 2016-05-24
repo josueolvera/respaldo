9a1e2b35ad661b917f8201975fa9049fc4441978
Josue Olvera <josue.olvera.r@gmail.com>
2016-05-23 19:02:42 -0500

    Agrega capas CTransactionsStatus

---
+++ src/main/java/mx/bidg/dao/CTransactionsStatusDao.java
@@ -0,0 +1,9 @@
+package mx.bidg.dao;
+
+import mx.bidg.model.CTransactionsStatus;
+
+/**
+ * Created by jolvera on 23/05/16.
+ */
+public interface CTransactionsStatusDao extends InterfaceDao<CTransactionsStatus> {
+}
