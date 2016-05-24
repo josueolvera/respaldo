98b4f281980022fafbd589b0d953c44d995964ce
Josue Olvera <josue.olvera.r@gmail.com>
2016-05-23 19:01:42 -0500

    Agrega capas CAccountsPayable

---
+++ src/main/java/mx/bidg/service/CAccountsPayableStatusService.java
@@ -0,0 +1,12 @@
+package mx.bidg.service;
+
+import mx.bidg.model.CAccountsPayableStatus;
+
+import java.util.List;
+
+/**
+ * Created by jolvera on 23/05/16.
+ */
+public interface CAccountsPayableStatusService {
+    List<CAccountsPayableStatus> findAll();
+}
