52820c223776aaa3a78f47090e1d228014ccb360
Gerardo8 <gerardo.lopezre@gmail.com>
2016-06-10 11:32:00 -0500

    Se agrega atributo 'layoutFileName' a la clase 'CSapFile'

--- src/main/java/mx/bidg/model/CSapFile.java
+++ src/main/java/mx/bidg/model/CSapFile.java
@@ -58,6 +58,13 @@ public class CSapFile implements Serializable {
 
     @Basic(optional = false)
     @NotNull
+    @Size(min = 1, max = 1024)
+    @Column(name = "LAYOUT_FILE_NAME")
+    @JsonView(JsonViews.Root.class)
+    private String layoutFileName;
+
+    @Basic(optional = false)
+    @NotNull
     @Column(name = "LAST_UPLOADED_DATE")
     @Convert(converter = DateTimeConverter.class)
     @JsonView(JsonViews.Root.class)
