757b5a1a0af0985e55046fe6ceee3bdb3294bc1f
Gerardo8 <gerardo.lopezre@gmail.com>
2016-06-10 11:11:49 -0500

    Se eliminan import sin uso en la CTransactionsDocumentsTypes

--- src/main/java/mx/bidg/model/CTransactionsDocumentsTypes.java
+++ src/main/java/mx/bidg/model/CTransactionsDocumentsTypes.java
@@ -16,11 +16,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
