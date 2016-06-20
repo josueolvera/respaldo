555343321900876ec65b203cd122a01dc4a6cfa5
Gerardo8 <gerardo.lopezre@gmail.com>
2016-06-20 17:03:18 -0500

    Cambio del nombre de metodo setRegion a SetRegionName

--- src/main/java/mx/bidg/service/impl/SapSaleServiceImpl.java
+++ src/main/java/mx/bidg/service/impl/SapSaleServiceImpl.java
@@ -272,7 +272,7 @@ public class SapSaleServiceImpl implements SapSaleService {
                     }
                     if (payments != null) newSapSale.setPayments(payments.getStringCellValue());
                     if (product != null) newSapSale.setProduct(product.getStringCellValue());
-                    if (region != null) newSapSale.setRegion(region.getStringCellValue());
+                    if (region != null) newSapSale.setRegionName(region.getStringCellValue());
                     if (requestedAmount != null) {
                         BigDecimal bdRequestedAmount = new BigDecimal(requestedAmount.getNumericCellValue());
                         newSapSale.setRequestedAmount(bdRequestedAmount);
