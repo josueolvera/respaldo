072fc04c31891b2945fe7810e605b684d351a44f
Gerardo8 <gerardo.lopezre@gmail.com>
2016-06-20 17:01:59 -0500

    Cambio del nombre de metodo setRegion a SetRegionName

--- src/main/java/mx/bidg/service/impl/SapSaleServiceImpl.java
+++ src/main/java/mx/bidg/service/impl/SapSaleServiceImpl.java
@@ -112,7 +112,7 @@ public class SapSaleServiceImpl implements SapSaleService {
             }
             if (payments != null) sapSale.setPayments(payments.getStringCellValue());
             if (product != null) sapSale.setProduct(product.getStringCellValue());
-            if (region != null) sapSale.setRegion(region.getStringCellValue());
+            if (region != null) sapSale.setRegionName(region.getStringCellValue());
             if (requestedAmount != null) {
                 BigDecimal bdRequestedAmount = new BigDecimal(requestedAmount.getNumericCellValue());
                 sapSale.setRequestedAmount(bdRequestedAmount);
@@ -221,7 +221,7 @@ public class SapSaleServiceImpl implements SapSaleService {
                     }
                     if (payments != null) sapSale.setPayments(payments.getStringCellValue());
                     if (product != null) sapSale.setProduct(product.getStringCellValue());
-                    if (region != null) sapSale.setRegion(region.getStringCellValue());
+                    if (region != null) sapSale.setRegionName(region.getStringCellValue());
                     if (requestedAmount != null) {
                         BigDecimal bdRequestedAmount = new BigDecimal(requestedAmount.getNumericCellValue());
                         sapSale.setRequestedAmount(bdRequestedAmount);
