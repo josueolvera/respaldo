14c61effec5287fe67f8f38db2601cfa0d496f1e
Josue Olvera <josue.olvera.r@gmail.com>
2016-06-20 18:08:36 -0500

    Agrega metodo de persistencia de tabla pibote al recibir el agreement y el dwentrerprise

--- src/main/java/mx/bidg/controller/CAgreementsController.java
+++ src/main/java/mx/bidg/controller/CAgreementsController.java
@@ -5,13 +5,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import mx.bidg.config.JsonViews;
 import mx.bidg.model.CAgreements;
+import mx.bidg.model.DwEnterprises;
+import mx.bidg.model.DwEnterprisesAgreements;
 import mx.bidg.service.CAgreementsService;
+import mx.bidg.service.DwEnterprisesAgreementsService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,6 +33,9 @@ public class CAgreementsController {
 
     @Autowired
     CAgreementsService cAgreementsService;
+
+    @Autowired
+    DwEnterprisesAgreementsService dwEnterprisesAgreementsService;
 
     ObjectMapper mapper = new ObjectMapper().registerModule(new Hibernate4Module());
 
@@ -55,5 +62,22 @@ public class CAgreementsController {
         }else {
             return new ResponseEntity<>("El nombre ya existe", HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @RequestMapping(value = "/{idAgreement}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity <String> saveDwEnterpriseAgreement(@PathVariable Integer idAgreement, @RequestBody String data) throws IOException{
+        CAgreements agreement = cAgreementsService.findById(idAgreement);
+        JsonNode node = mapper.readTree(data);
+        DwEnterprises dwEnterprise = new DwEnterprises(node.get("idDwEnterprise").asInt());
+
+        DwEnterprisesAgreements dwEnterprisesAgreements = new DwEnterprisesAgreements();
+
+        dwEnterprisesAgreements.setDwEnterprise(dwEnterprise);
+        dwEnterprisesAgreements.setAgreement(agreement);
+        dwEnterprisesAgreements.setIdAccessLevel(1);
+
+        dwEnterprisesAgreementsService.save(dwEnterprisesAgreements);
+
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(dwEnterprisesAgreements),HttpStatus.OK);
     }
 }
