8ceef87c257e1c36933a6e0daba249ad89ecfad3
Gerardo8 <gerardo.lopezre@gmail.com>
2016-06-20 16:12:14 -0500

    Agrega metodo para actualizar CBranchs en CBranchsController

--- src/main/java/mx/bidg/controller/CBrachsController.java
+++ src/main/java/mx/bidg/controller/CBrachsController.java
@@ -5,18 +5,19 @@
  */
 package mx.bidg.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import mx.bidg.config.JsonViews;
-import mx.bidg.model.CBranchs;
+import mx.bidg.model.*;
 import mx.bidg.service.CBranchsService;
 import mx.bidg.service.DwEnterprisesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -49,6 +50,45 @@ public class CBrachsController {
         branch = cBranchsService.save(branch, idDistributor, idRegion);
         return map.writerWithView(JsonViews.Root.class).writeValueAsString(branch);
     }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    private @ResponseBody String update(@RequestBody String data) throws Exception {
+        JsonNode node = map.readTree(data);
+
+        int idBranch = node.get("idBranch").asInt();
+        int idDistributor = node.get("idDistributor").asInt();
+        int idRegion = node.get("idRegion").asInt();
+        int idDwEnterprise = node.get("idDwEnterprise").asInt();
+
+        CBranchs cBranchs = cBranchsService.findById(idBranch);
+        DwEnterprises currentDwEnterprises =
+                dwEnterprisesService.findById(idDwEnterprise);
+
+        CBranchs oldBranch = currentDwEnterprises.getBranch();
+        CDistributors oldDistributor = currentDwEnterprises.getDistributor();
+        CRegions oldRegion = currentDwEnterprises.getRegion();
+        CGroups oldGroup = currentDwEnterprises.getGroup();
+        CAreas oldArea = currentDwEnterprises.getArea();
+        Integer oldBudgetable = currentDwEnterprises.getBudgetable();
+
+        currentDwEnterprises.setDistributor(new CDistributors(idDistributor));
+        currentDwEnterprises.setRegion(new CRegions(idRegion));
+        currentDwEnterprises.setStatus(true);
+        dwEnterprisesService.update(currentDwEnterprises);
+
+        DwEnterprises dwEnterprises = new DwEnterprises();
+        dwEnterprises.setBranch(oldBranch);
+        dwEnterprises.setDistributor(oldDistributor);
+        dwEnterprises.setRegion(oldRegion);
+        dwEnterprises.setGroup(oldGroup);
+        dwEnterprises.setArea(oldArea);
+        dwEnterprises.setBudgetable(oldBudgetable);
+        dwEnterprises.setStatus(false);
+        dwEnterprisesService.save(dwEnterprises);
+
+        return map.writerWithView(JsonViews.Root.class).writeValueAsString(cBranchs);
+    }
+
     @RequestMapping(value = "/change-branch-status", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     private @ResponseBody String changeBranchStatus(@RequestBody Integer idBranch) throws Exception {
         CBranchs branch = cBranchsService.changeBranchStatus(idBranch);
