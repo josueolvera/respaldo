4ee09a142271326434b23a127add204c894f0f96
Josue Olvera <josue.olvera.r@gmail.com>
2016-06-24 18:19:53 -0500

    Agrega controlador para metodo de busqueda por areas en CAreas

--- src/main/java/mx/bidg/controller/CAreasController.java
+++ src/main/java/mx/bidg/controller/CAreasController.java
@@ -6,7 +6,11 @@
 package mx.bidg.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 import java.util.List;
+
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import mx.bidg.config.JsonViews;
 import mx.bidg.model.CAreas;
 import mx.bidg.model.CGroups;
@@ -14,6 +18,7 @@ import mx.bidg.model.DwEnterprises;
 import mx.bidg.service.CAreasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,16 +37,22 @@ public class CAreasController {
     @Autowired
     CAreasService cAreasService;
     
-    ObjectMapper map = new ObjectMapper();
+    ObjectMapper map = new ObjectMapper().registerModule(new Hibernate4Module());
     
     @RequestMapping(produces = "application/json;charset=UTF-8")
     public @ResponseBody String getAreas() throws Exception {
         List<CAreas> list = cAreasService.findAll();
-        return map.writerWithView(JsonViews.Root.class).writeValueAsString(list);
+        return map.writerWithView(JsonViews.Embedded.class).writeValueAsString(list);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody String findSapSaleById(@PathVariable int id) throws Exception {
         return map.writerWithView(JsonViews.Root.class).writeValueAsString(cAreasService.findById(id));
+    }
+
+    @RequestMapping(value = "/area-role/{idArea}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> getAreasWithRoles(@PathVariable Integer idArea)throws IOException{
+        CAreas areas = cAreasService.findAreaWhitRole(idArea);
+        return new ResponseEntity<>(map.writerWithView(JsonViews.Embedded.class).writeValueAsString(areas),HttpStatus.OK);
     }
 }
