487be6f1ec3cb6bad5e1256be0170284dde25f54
Gerardo8 <gerardo.lopezre@gmail.com>
2016-06-27 18:31:10 -0500

    Agrega clase EmployeesHistoryDao

--- src/main/java/mx/bidg/service/impl/EmployeesHistoryServiceImpl.java
+++ src/main/java/mx/bidg/service/impl/EmployeesHistoryServiceImpl.java
@@ -1,10 +1,13 @@
 package mx.bidg.service.impl;
 
 import mx.bidg.dao.EmployeesHistoryDao;
+import mx.bidg.model.EmployeesHistory;
 import mx.bidg.service.EmployeesHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by gerardo8 on 24/06/16.
@@ -15,4 +18,19 @@ public class EmployeesHistoryServiceImpl implements EmployeesHistoryService {
 
     @Autowired
     private EmployeesHistoryDao employeesHistoryDao;
+
+    @Override
+    public EmployeesHistory findById(Integer id) {
+        return employeesHistoryDao.findById(id);
+    }
+
+    @Override
+    public List<EmployeesHistory> findAll() {
+        return employeesHistoryDao.findAll();
+    }
+
+    @Override
+    public List<EmployeesHistory> findByDistributorAndRegionAndBranchAndAreaAndRole(Integer idDistributor, Integer idRegion, Integer idBranch, Integer idArea, Integer idRole) {
+        return employeesHistoryDao.findByDistributorAndRegionAndBranchAndAreaAndRole(idDistributor,idRegion,idBranch,idArea,idRole);
+    }
 }
