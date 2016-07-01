b9773c5c8bfa55c4a3781bc226ab37a88f65a9cb
rubensve <ruben_veloz89@hotmail.com>
2016-07-01 17:36:03 -0500

    Se agrega clase C_Regions Controller

---
+++ src/main/java/mx/bidg/controller/CRegionsController.java
@@ -0,0 +1,32 @@
+package mx.bidg.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import static com.google.common.io.Files.map;
+import java.util.List;
+import mx.bidg.config.JsonViews;
+import mx.bidg.model.CRegions;
+import mx.bidg.service.CRegionsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author rubens
+ */
+@Controller
+@RequestMapping("/regions")
+public class CRegionsController {
+    @Autowired
+    CRegionsService cRegionsService;
+    
+    ObjectMapper map = new ObjectMapper();
+    
+    @RequestMapping(produces = "application/json;charset=UTF-8")
+    public @ResponseBody String getRegions() throws Exception {
+        List<CRegions> list = cRegionsService.findAll();
+        return map.writerWithView(JsonViews.Root.class).writeValueAsString(list);
+    }
+    
+}
