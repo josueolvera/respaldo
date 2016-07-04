e2c40b7c58c3cec9ea6a56c1bf1a33bbe45fbbe9
Josue Olvera <josue.olvera.r@gmail.com>
2016-07-04 11:16:14 -0500

    Agrega metodo de persistencia de Empleado y persistencia de empleado en employees history y persistencia de archivos de empleado

--- src/main/java/mx/bidg/controller/EmployeesController.java
+++ src/main/java/mx/bidg/controller/EmployeesController.java
@@ -4,23 +4,28 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import mx.bidg.config.JsonViews;
+import mx.bidg.exceptions.ValidationException;
 import mx.bidg.model.*;
 import mx.bidg.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Rafael Viveros
@@ -45,6 +50,33 @@ public class EmployeesController {
     @Autowired
     private DwEmployeesService dwEmployeesService;
 
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private EmployeeDocumentsService employeeDocumentsService;
+
+    @Autowired
+    private EmployeesHistoryService employeesHistoryService;
+
+    @Autowired
+    private CEmployeeDocumentsTypesService cEmployeeDocumentsTypesService;
+
+    @Autowired
+    private CEducationService cEducationService;
+
+    @Autowired
+    private CStatusMaritalService cStatusMaritalService;
+
+    @Autowired
+    private CBanksService cBanksService;
+
+    @Autowired
+    private CCurrenciesService currenciesService;
+
+    @Autowired
+    private CRolesService cRolesService;
+
     private ObjectMapper mapper = new ObjectMapper().registerModule(new Hibernate4Module());
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -64,6 +96,12 @@ public class EmployeesController {
                 mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(employees),
                 HttpStatus.OK
         );
+    }
+
+    @RequestMapping(value = "/{idEmployee}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> findById(@PathVariable Integer idEmployee) throws IOException{
+        Employees employee = employeesService.findById(idEmployee);
+        return  new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(employee), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -93,7 +131,7 @@ public class EmployeesController {
         employee.setParentalLast(jnode.get("parentalLast").asText());
         employee.setMotherLast(jnode.get("motherLast").asText());
         employee.setPostcode(jnode.get("postcode").asText());
-        employee.setEducation(new CEducation(jnode.get("idEducation").asInt()));
+        employee.setEducation(cEducationService.findById(jnode.get("idEducation").asInt()));
         employee.setEmployeeType(jnode.get("employeeType").asInt());
         employee.setSize(jnode.get("size").asText());
         employee.setSizeNumber(jnode.get("sizeNumber").asInt());
@@ -104,7 +142,7 @@ public class EmployeesController {
         employee.setHomePhone(jnode.get("homePhone").asText());
         employee.setImss(jnode.get("imss").asText());
         employee.setInfonavitNumber(jnode.get("infonavitNumber").asText());
-        employee.setStatusMarital(new CStatusMarital(jnode.get("idStatusMarital").asInt()));
+        employee.setStatusMarital(cStatusMaritalService.findById(jnode.get("idStatusMarital").asInt()));
         employee.setSalary(jnode.get("salary").decimalValue());
         employee.setStatus(1);
         employee.setMail(jnode.get("mail").asText());
@@ -119,8 +157,8 @@ public class EmployeesController {
             Accounts account = new Accounts();
             account.setAccountNumber(node.get("accountNumber").asText());
             account.setAccountClabe(node.get("accountClabe").asText());
-            account.setBank(new CBanks(node.get("idBank").asInt()));
-            account.setCurrencies(new CCurrencies(node.get("idCurrency").asInt()));
+            account.setBank(cBanksService.findById(node.get("idBank").asInt()));
+            account.setCurrencies(currenciesService.geById(node.get("idCurrency").asInt()));
             account.setAccountType(CAccountsTypes.DEFINITIVA);
             account.setIdAccessLevel(1);
 
@@ -135,12 +173,120 @@ public class EmployeesController {
         DwEmployees dwEmployees = new DwEmployees();
 
         dwEmployees.setEmployee(employee);
-        dwEmployees.setDwEnterprise(new DwEnterprises(jnode.get("dwEmployees").get("area").get("dwEnterprise").get("idDwEnterprise").asInt()));
-        dwEmployees.setRole(new CRoles(jnode.get("dwEmployees").get("role").asInt()));
+        dwEmployees.setDwEnterprise(dwEnterprisesService.findById(jnode.get("dwEmployees").get("area").get("dwEnterprise").get("idDwEnterprise").asInt()));
+        dwEmployees.setRole(cRolesService.findById(jnode.get("dwEmployees").get("role").asInt()));
         dwEmployees.setCreationDate(LocalDateTime.now());
         dwEmployeesService.save(dwEmployees);
 
+        CActionTypes cActionType = CActionTypes.ALTA;
+        employeesHistoryService.save(dwEmployees,cActionType);
 
         return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(employee), HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/{idEmployee}/attachments", method = RequestMethod.POST)
+    public ResponseEntity<String> attachDocuments(@PathVariable Integer idEmployee, HttpServletRequest request) throws Exception {
+        saveAttachDocuments(idEmployee, request);
+        return new ResponseEntity<>("Registro exitoso", HttpStatus.OK);
+    }
+
+    private EmployeeDocuments findDocument(Integer idDocumentType, List<EmployeeDocuments> documents) {
+        if (documents.isEmpty()) {
+            return null;
+        }
+        for (EmployeeDocuments document : documents) {
+            if (document.getIdDocumentType().equals(idDocumentType)) {
+                return document;
+            }
+        }
+
+        return null;
+    }
+
+    private void saveAttachDocuments(Integer idEmployee, HttpServletRequest request) throws Exception{
+        String SAVE_PATH = env.getRequiredProperty("employees.documents_dir");
+        String[] fileMediaTypes = env.getRequiredProperty("employees.attachments.media_types").split(",");
+        List<EmployeeDocuments> documents = employeeDocumentsService.findByIdEmployee(idEmployee);
+        Pattern pattern = Pattern.compile("(\\d+)");
+
+        for (Part filePart: request.getParts()) {
+            boolean isValidMediaType = false;
+
+            if (! filePart.getName().matches("^file-type-[0-12]+$")) {
+                continue;
+            }
+            if (filePart.getSize() <= 0) {
+                continue;
+            }
+
+            for (String mediaType : fileMediaTypes) {
+                if (filePart.getContentType().equals(mediaType)) {
+                    isValidMediaType = true;
+                    break;
+                }
+            }
+
+            if (! isValidMediaType) {
+                throw new ValidationException("Tipo de archivo no admitido", "Tipo de archivo no admitido");
+            }
+
+            Matcher matcher = pattern.matcher(filePart.getName());
+            matcher.find();
+            Integer idDocumentType = Integer.parseInt(matcher.group(0));
+
+            String destDir = "/employee_" + idEmployee;
+            String destFile = destDir + "/Documento." + idDocumentType +
+                    "." + Calendar.getInstance().getTimeInMillis();
+
+            File dir = new File(SAVE_PATH + destDir);
+            if (! dir.exists()) {
+                dir.mkdir();
+            }
+
+            OutputStream outputStream = new FileOutputStream(new File(SAVE_PATH + destFile));
+            InputStream inputStream = filePart.getInputStream();
+            int read;
+            byte[] buffer = new byte[1024];
+            while ((read = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, read);
+            }
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+
+            EmployeeDocuments document = new EmployeeDocuments();
+            document.setEmployee(new Employees(idEmployee));
+            document.setcDocumentType(new CEmployeeDocumentsTypes(idDocumentType));
+            document.setDocumentUrl(destFile);
+            document.setDocumentName(filePart.getSubmittedFileName());
+            document.setUploadingDate(LocalDateTime.now());
+            document.setCurrentDocument(1);
+            document.setIdAccessLevel(1);
+            employeeDocumentsService.save(document);
+            saveNameField(document.getIdDocument(), request);
+
+            EmployeeDocuments oldDocument = this.findDocument(idDocumentType, documents);
+            if (oldDocument != null) {
+                oldDocument.setCurrentDocument(0);
+                employeeDocumentsService.update(oldDocument);
+            }
+        }
+    }
+    private void saveNameField(Integer idDocument, HttpServletRequest request){
+        EmployeeDocuments document = employeeDocumentsService.findById(idDocument);
+        List<CEmployeeDocumentsTypes> documentsTypes = cEmployeeDocumentsTypesService.findByInput();
+        CEmployeeDocumentsTypes cEmployeeDocumentsTypes = document.getcDocumentType();
+
+        for(CEmployeeDocumentsTypes documentsType : documentsTypes) {
+            if (documentsType.getIdDocumentType() == cEmployeeDocumentsTypes.getIdDocumentType()) {
+                    String[] paramValues = request.getParameterValues(documentsType.getDocumentName());
+                    for (int i = 0; i < paramValues.length; i++) {
+                        String paramValue = paramValues[i];
+                        document.setField(paramValue);
+                        employeeDocumentsService.update(document);
+                    }
+            }
+        }
+    }
+
 }
