b72e023c0e2c26be8590650a1d4c225497a20dcf
josue olvera <josue.olvera.r@gmail.com>
2016-07-08 10:41:55 -0500

    Agrega controlador de CAccountingAccountType

---
+++ src/main/java/mx/bidg/controller/CAccountingAccountTypeController.java
@@ -0,0 +1,37 @@
+package mx.bidg.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import mx.bidg.config.JsonViews;
+import mx.bidg.model.CAccountingAccountType;
+import mx.bidg.service.CAccountingAccountTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by josue on 7/07/16.
+ */
+@Controller
+@RequestMapping("accounting-type")
+public class CAccountingAccountTypeController {
+
+    @Autowired
+    CAccountingAccountTypeService cAccountingAccountTypeService;
+
+    ObjectMapper mapper = new ObjectMapper().registerModule(new Hibernate4Module());
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> findAll() throws IOException{
+        List<CAccountingAccountType> accountingAccountTypes = cAccountingAccountTypeService.findAll();
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Root.class).writeValueAsString(accountingAccountTypes), HttpStatus.OK);
+    }
+
+}
