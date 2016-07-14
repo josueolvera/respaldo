9ef4dd71669b8efc548291115bb1107f03ee0112
josueolvera <josue.olvera.r@gmail.com>
2016-07-14 09:49:43 -0500

    Agrega dao CTravelExpensesConceptsDao

---
+++ src/main/java/mx/bidg/dao/CTravelExpensesConceptsDao.java
@@ -0,0 +1,9 @@
+package mx.bidg.dao;
+
+import mx.bidg.model.CTravelExpensesConcepts;
+
+/**
+ * Created by josueolvera on 13/07/16.
+ */
+public interface CTravelExpensesConceptsDao extends InterfaceDao<CTravelExpensesConcepts> {
+}
---
+++ src/main/java/mx/bidg/dao/impl/CTravelExpensesConceptsDaoImpl.java
@@ -0,0 +1,43 @@
+package mx.bidg.dao.impl;
+
+import mx.bidg.dao.AbstractDao;
+import mx.bidg.dao.CTravelExpensesConceptsDao;
+import mx.bidg.model.CTravelExpensesConcepts;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by josueolvera on 13/07/16.
+ */
+@Repository
+public class CTravelExpensesConceptsDaoImpl extends AbstractDao<Integer, CTravelExpensesConcepts> implements CTravelExpensesConceptsDao {
+
+    @Override
+    public CTravelExpensesConcepts save(CTravelExpensesConcepts entity) {
+        persist(entity);
+        return entity;
+    }
+
+    @Override
+    public CTravelExpensesConcepts findById(int id) {
+        return getByKey(id);
+    }
+
+    @Override
+    public List<CTravelExpensesConcepts> findAll() {
+        return (List<CTravelExpensesConcepts>) createEntityCriteria().list();
+    }
+
+    @Override
+    public CTravelExpensesConcepts update(CTravelExpensesConcepts entity) {
+        modify(entity);
+        return entity;
+    }
+
+    @Override
+    public boolean delete(CTravelExpensesConcepts entity) {
+        remove(entity);
+        return true;
+    }
+}
