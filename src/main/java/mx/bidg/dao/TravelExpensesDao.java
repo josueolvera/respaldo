186f06c15f3708f2949d2715d3d9d01d96d59d40
josueolvera <josue.olvera.r@gmail.com>
2016-07-14 09:48:03 -0500

    Agrega dao TravelExpensesDao

---
+++ src/main/java/mx/bidg/dao/TravelExpensesDao.java
@@ -0,0 +1,9 @@
+package mx.bidg.dao;
+
+import mx.bidg.model.TravelExpenses;
+
+/**
+ * Created by josueolvera on 13/07/16.
+ */
+public interface TravelExpensesDao extends InterfaceDao<TravelExpenses> {
+}
---
+++ src/main/java/mx/bidg/dao/impl/TravelExpensesDaoImpl.java
@@ -0,0 +1,43 @@
+package mx.bidg.dao.impl;
+
+import mx.bidg.dao.AbstractDao;
+import mx.bidg.dao.TravelExpensesDao;
+import mx.bidg.model.TravelExpenses;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by josueolvera on 13/07/16.
+ */
+@Repository
+public class TravelExpensesDaoImpl extends AbstractDao<Integer,TravelExpenses> implements TravelExpensesDao {
+
+    @Override
+    public TravelExpenses save(TravelExpenses entity) {
+        persist(entity);
+        return entity;
+    }
+
+    @Override
+    public TravelExpenses findById(int id) {
+        return getByKey(id);
+    }
+
+    @Override
+    public List<TravelExpenses> findAll() {
+        return (List<TravelExpenses>) createEntityCriteria().list();
+    }
+
+    @Override
+    public TravelExpenses update(TravelExpenses entity) {
+        modify(entity);
+        return entity;
+    }
+
+    @Override
+    public boolean delete(TravelExpenses entity) {
+        remove(entity);
+        return true;
+    }
+}
