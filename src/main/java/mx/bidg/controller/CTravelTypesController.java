9cee707e3c3646ab314e70d23e121c985565baf2
josueolvera <josue.olvera.r@gmail.com>
2016-07-14 09:59:26 -0500

    Agrega controlador CTravelTypesController

---
+++ src/main/java/mx/bidg/controller/CTravelTypesController.java
@@ -0,0 +1,12 @@
+package mx.bidg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by josueolvera on 14/07/16.
+ */
+@Controller
+@RequestMapping("travel-types")
+public class CTravelTypesController {
+}
