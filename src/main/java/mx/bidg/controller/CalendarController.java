17974be3251722058ae916fc1b22c0c09320d7d1
rubensve <ruben_veloz89@hotmail.com>
2016-07-18 17:49:34 -0500

    Vistas y controlador de agenda

---
+++ src/main/java/mx/bidg/controller/CalendarController.java
@@ -0,0 +1,30 @@
+package mx.bidg.controller;
+
+import mx.bidg.model.CRequestsCategories;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author rubens
+ */
+
+@Controller
+@RequestMapping("/calendar")
+public class CalendarController {
+    
+    @RequestMapping(value = "/rooms",method = RequestMethod.GET, produces = "text/html;charset=UTF-8")
+    public ModelAndView roomsView() {
+        ModelAndView model = new ModelAndView();
+        //model.addObject("idRefund", idRefund);
+        //model.addObject("cat", CRequestsCategories.REEMBOLSOS);
+        model.setViewName("Rooms");
+        return model;
+    }
+    
+
+    
+}
---
+++ src/main/webapp/WEB-INF/Rooms.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : Rooms
+    Created on : 18/07/2016, 05:41:38 PM
+    Author     : rubens
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
