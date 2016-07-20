69fcd64b3a3809c624171e4e071e060bed47fd47
josueolvera <josue.olvera.r@gmail.com>
2016-07-20 08:44:44 -0500

    Agrega CEmployeeTypeDao con su respectiva implementacion

---
+++ src/main/java/mx/bidg/dao/CEmployeeTypeDao.java
@@ -0,0 +1,9 @@
+package mx.bidg.dao;
+
+import mx.bidg.model.CEmployeeType;
+
+/**
+ * Created by josueolvera on 19/07/16.
+ */
+public interface CEmployeeTypeDao extends InterfaceDao<CEmployeeType> {
+}
