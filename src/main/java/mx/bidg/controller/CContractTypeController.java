b4e2bd1c14f074f4688b3a5c4dcdb2c48e617de3
josueolvera <josue.olvera.r@gmail.com>
2016-07-20 08:54:31 -0500

    Agrega controlador de tipo de contrato en contractType

---
+++ src/main/java/mx/bidg/controller/CContractTypeController.java
@@ -0,0 +1,36 @@
+package mx.bidg.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import mx.bidg.config.JsonViews;
+import mx.bidg.model.CContractType;
+import mx.bidg.service.CContractTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by josueolvera on 19/07/16.
+ */
+@Controller
+@RequestMapping("contract-type")
+public class CContractTypeController {
+
+    @Autowired
+    CContractTypeService cContractTypeService;
+
+    ObjectMapper mapper = new ObjectMapper().registerModule(new Hibernate4Module());
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> findAll() throws IOException{
+        List<CContractType> cContractTypes = cContractTypeService.findAll();
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(cContractTypes), HttpStatus.OK);
+    }
+}
