26dd53e8267639559dab530593e25156f42c5050
josueolvera <josue.olvera.r@gmail.com>
2016-07-20 08:45:31 -0500

    Agrega CGendersService con su respectiva implementaciòn

---
+++ src/main/java/mx/bidg/service/CGendersService.java
@@ -0,0 +1,13 @@
+package mx.bidg.service;
+
+import mx.bidg.model.CGenders;
+
+import java.util.List;
+
+/**
+ * Created by josueolvera on 19/07/16.
+ */
+public interface CGendersService {
+    CGenders findById(Integer idGender);
+    List<CGenders> findAll();
+}
---
+++ src/main/java/mx/bidg/service/impl/CGendersServiceImpl.java
@@ -0,0 +1,31 @@
+package mx.bidg.service.impl;
+
+import mx.bidg.dao.CGendersDao;
+import mx.bidg.model.CGenders;
+import mx.bidg.service.CGendersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by josueolvera on 19/07/16.
+ */
+@Service
+@Transactional
+public class CGendersServiceImpl implements CGendersService {
+
+    @Autowired
+    CGendersDao cGendersDao;
+
+    @Override
+    public CGenders findById(Integer idGender) {
+        return cGendersDao.findById(idGender);
+    }
+
+    @Override
+    public List<CGenders> findAll() {
+        return cGendersDao.findAll();
+    }
+}
