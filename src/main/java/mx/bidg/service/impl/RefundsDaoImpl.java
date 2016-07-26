089faeb24377948d783a573b244b8449f389dbf3
Gerardo8 <gerardo.lopezre@gmail.com>
2016-07-26 09:38:11 -0500

    Agrega clase RefundsDaoImpl

---
+++ src/main/java/mx/bidg/service/impl/RefundsDaoImpl.java
@@ -0,0 +1,43 @@
+package mx.bidg.service.impl;
+
+import mx.bidg.dao.AbstractDao;
+import mx.bidg.dao.RefundsDao;
+import mx.bidg.model.Refunds;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by gerardo8 on 25/07/16.
+ */
+@SuppressWarnings("unchecked")
+@Repository
+public class RefundsDaoImpl extends AbstractDao<Integer,Refunds> implements RefundsDao {
+    @Override
+    public Refunds save(Refunds entity) {
+        persist(entity);
+        return entity;
+    }
+
+    @Override
+    public Refunds findById(int id) {
+        return getByKey(id);
+    }
+
+    @Override
+    public List<Refunds> findAll() {
+        return createEntityCriteria().list();
+    }
+
+    @Override
+    public Refunds update(Refunds entity) {
+        modify(entity);
+        return entity;
+    }
+
+    @Override
+    public boolean delete(Refunds entity) {
+        remove(entity);
+        return true;
+    }
+}
