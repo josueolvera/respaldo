a1a747491dc6075cf16657b4ac61c0862fd0b166
Gerardo8 <gerardo.lopezre@gmail.com>
2016-07-26 09:43:16 -0500

    Agrega clase RegimenFiscal

---
+++ src/main/java/mx/bidg/pojos/xml_bill/RegimenFiscal.java
@@ -0,0 +1,19 @@
+package mx.bidg.pojos.xml_bill;
+
+import java.io.Serializable;
+
+/**
+ * Created by gerardo8 on 21/07/16.
+ */
+public class RegimenFiscal implements Serializable {
+
+    private String regimen;
+
+    public String getRegimen() {
+        return regimen;
+    }
+
+    public void setRegimen(String regimen) {
+        this.regimen = regimen;
+    }
+}
