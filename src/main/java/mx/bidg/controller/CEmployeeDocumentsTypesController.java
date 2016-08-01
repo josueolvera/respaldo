e70692a59e097285e1db30a7c16819850a0b5122
josueolvera <josue.olvera.r@gmail.com>
2016-08-01 09:17:46 -0500

    Agrega controlador de busqueda de documentos por sucursal

--- src/main/java/mx/bidg/controller/CEmployeeDocumentsTypesController.java
+++ src/main/java/mx/bidg/controller/CEmployeeDocumentsTypesController.java
@@ -4,7 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import mx.bidg.config.JsonViews;
 import mx.bidg.model.CEmployeeDocumentsTypes;
+import mx.bidg.model.DitributorsEmployeeDocuments;
+import mx.bidg.model.DwEnterprises;
 import mx.bidg.service.CEmployeeDocumentsTypesService;
+import mx.bidg.service.DistributorsEmployeeDocumentsService;
+import mx.bidg.service.DwEnterprisesService;
 import org.apache.xmlbeans.impl.xb.xsdschema.ListDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -26,6 +31,12 @@ public class CEmployeeDocumentsTypesController {
 
     @Autowired
     CEmployeeDocumentsTypesService cEmployeeDocumentsTypesService;
+
+    @Autowired
+    DwEnterprisesService dwEnterprisesService;
+
+    @Autowired
+    DistributorsEmployeeDocumentsService distributorsEmployeeDocumentsService;
 
     ObjectMapper mapper = new ObjectMapper().registerModule(new Hibernate4Module());
 
@@ -39,5 +50,12 @@ public class CEmployeeDocumentsTypesController {
     public ResponseEntity<String> findByEmployee(@PathVariable Integer idEmployee) throws Exception{
         List<CEmployeeDocumentsTypes> documentsTypes = cEmployeeDocumentsTypesService.findByEmployee(idEmployee);
         return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(documentsTypes), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{idBranch}/{idArea}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> findByDistributor(@PathVariable Integer idBranch, @PathVariable Integer idArea) throws IOException{
+        DwEnterprises dwEnterprise = dwEnterprisesService.findByBranchAndArea(idBranch,idArea);
+        List<DitributorsEmployeeDocuments> distributorsDocuments = distributorsEmployeeDocumentsService.findByDistributor(dwEnterprise.getIdDistributor());
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(distributorsDocuments), HttpStatus.OK);
     }
 }
