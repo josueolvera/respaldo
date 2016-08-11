e2e17fc20eeb9d4f115cfc0cb95a52b282b3c2e4
josueolvera <josue.olvera.r@gmail.com>
2016-08-11 13:35:35 -0500

    Agrega GroupsAgreementsDao con su respectia implementacion

---
+++ src/main/java/mx/bidg/dao/impl/GroupsAgreementsDaoImpl.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.bidg.dao.impl;
+
+import java.util.LinkedList;
+import java.util.List;
+import mx.bidg.dao.AbstractDao;
+import mx.bidg.dao.GroupsAgreementsDao;
+import mx.bidg.model.GroupsAgreements;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GroupsAgreementsDaoImpl extends AbstractDao<Integer, GroupsAgreements> implements GroupsAgreementsDao {
+
+    @Override
+    public GroupsAgreements save(GroupsAgreements entity) {
+        persist(entity);
+        return entity;
+    }
+
+    @Override
+    public GroupsAgreements findById(int id) {
+        return getByKey(id);
+    }
+
+    @Override
+    public List<GroupsAgreements> findAll() {
+        return (List<GroupsAgreements>) createEntityCriteria().list();
+    }
+
+    @Override
+    public GroupsAgreements update(GroupsAgreements entity) {
+        modify(entity);
+        return entity;
+    }
+
+    @Override
+    public boolean delete(GroupsAgreements entity) {
+        remove(entity);
+        return true;
+    }
+    
+}
