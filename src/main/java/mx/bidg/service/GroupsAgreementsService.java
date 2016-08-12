a129427e423ac44b1f740e04e8e2feb5947ca66e
josueolvera <josue.olvera.r@gmail.com>
2016-08-12 11:42:53 -0500

    Agrega servicio GroupsAgreementsService con su respectiva implementacion

---
+++ src/main/java/mx/bidg/service/GroupsAgreementsService.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.bidg.service;
+
+import java.util.List;
+import mx.bidg.model.GroupsAgreements;
+
+/**
+ *
+ * @author josueolvera
+ */
+public interface GroupsAgreementsService {
+    GroupsAgreements save(GroupsAgreements groupsAgreements);
+    GroupsAgreements update(GroupsAgreements groupsAgreements);
+    GroupsAgreements findById(Integer idGa);
+    List<GroupsAgreements> findAll();
+    boolean delete (GroupsAgreements groupsAgreements);
+    
+}
