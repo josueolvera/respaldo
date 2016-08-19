69f9fcefd3126a9c6341c0f5379e55a71883b05d
josueolvera <josue.olvera.r@gmail.com>
2016-08-19 11:44:00 -0500

    Agrega Controlador de busqueda de rol por distribuidor y area

--- src/main/java/mx/bidg/controller/DistributorAreaRolServiceController.java
+++ src/main/java/mx/bidg/controller/DistributorAreaRolServiceController.java
@@ -1,20 +1,10 @@
 package mx.bidg.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import mx.bidg.config.JsonViews;
-import mx.bidg.model.DistributorAreaRol;
 import mx.bidg.service.DistributorAreaRolService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.io.IOException;
-import java.util.List;
 
 /**
  * Created by gerardo8 on 15/08/16.
@@ -28,10 +18,4 @@ public class DistributorAreaRolServiceController {
 
     @Autowired
     private ObjectMapper mapper;
-
-    @RequestMapping(value = "/{idDistributor}/{idArea}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<String> getRolByDistributorArea(@PathVariable Integer idDistributor, @PathVariable Integer idArea) throws IOException{
-        List<DistributorAreaRol> distributorAreaRoles = distributorAreaRolService.findRolByDistributorArea(idDistributor, idArea);
-        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(distributorAreaRoles), HttpStatus.OK);
-    }
 }
