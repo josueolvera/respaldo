1e89ce27ba93d668ee0d97ac2d2569f66843c157
josueolvera <josue.olvera.r@gmail.com>
2016-08-24 16:39:37 -0500

    Se corrige controlador findAll en CRegionsController

--- src/main/java/mx/bidg/controller/CRegionsController.java
+++ src/main/java/mx/bidg/controller/CRegionsController.java
@@ -2,14 +2,22 @@ package mx.bidg.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import static com.google.common.io.Files.map;
+
+import java.io.IOException;
 import java.util.List;
 import mx.bidg.config.JsonViews;
 import mx.bidg.model.CRegions;
 import mx.bidg.service.CRegionsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.imageio.IIOException;
 
 /**
  *
@@ -24,10 +32,10 @@ public class CRegionsController {
     @Autowired
     private ObjectMapper mapper;
     
-    @RequestMapping(produces = "application/json;charset=UTF-8")
-    public @ResponseBody String getRegions() throws Exception {
-        List<CRegions> list = cRegionsService.findAll();
-        return mapper.writerWithView(JsonViews.Root.class).writeValueAsString(list);
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> findAll()throws IOException{
+        List<CRegions> regionsList = cRegionsService.findAll();
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(regionsList), HttpStatus.OK);
     }
     
 }
