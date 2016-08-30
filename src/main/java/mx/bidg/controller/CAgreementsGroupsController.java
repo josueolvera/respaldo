bca80cd10c08c4181eab29327da104167135c94c
josueolvera <josue.olvera.r@gmail.com>
2016-08-30 18:45:42 -0500

    Agrega controlador de alta de grupo de convenio y controlador de baja logica de grupo de convenios

--- src/main/java/mx/bidg/controller/CAgreementsGroupsController.java
+++ src/main/java/mx/bidg/controller/CAgreementsGroupsController.java
@@ -5,16 +5,23 @@
  */
 package mx.bidg.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import mx.bidg.config.JsonViews;
+import mx.bidg.model.CAgreements;
 import mx.bidg.model.CAgreementsGroups;
+import mx.bidg.model.GroupsAgreements;
 import mx.bidg.service.CAgreementsGroupsService;
+import mx.bidg.service.CAgreementsService;
+import mx.bidg.service.GroupsAgreementsService;
 import org.hibernate.annotations.AnyMetaDef;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -33,12 +40,46 @@ public class CAgreementsGroupsController {
     CAgreementsGroupsService cAgreementsGroupsService;
 
     @Autowired
+    CAgreementsService agreementsService;
+
+    @Autowired
+    GroupsAgreementsService groupsAgreementsService;
+
+    @Autowired
     private ObjectMapper mapper;
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<String> findAll () throws IOException{
         List<CAgreementsGroups> agreementsGroups = cAgreementsGroupsService.findAll();
         return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(agreementsGroups), HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/new", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> saveGroupAgreements(@RequestBody String data) throws IOException{
+        JsonNode node = mapper.readTree(data);
+
+        CAgreementsGroups agreementsGroups = new CAgreementsGroups();
+        agreementsGroups.setAgreementGroupName(node.get("agreementGroupName").asText());
+        agreementsGroups = cAgreementsGroupsService.save(agreementsGroups);
+
+        List<CAgreements> agreementsList = agreementsService.findActives();
+
+        for (CAgreements agreements : agreementsList){
+            GroupsAgreements groupsAgreement = new GroupsAgreements();
+            groupsAgreement.setHasAgreement(false);
+            groupsAgreement.setAgreement(agreements);
+            groupsAgreement.setAgreementGroup(agreementsGroups);
+            groupsAgreement.setIdAccessLevel(1);
+            groupsAgreementsService.save(groupsAgreement);
+        }
+
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(agreementsGroups), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/low-group/{idAg}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> lowGroup(@PathVariable Integer idAg) throws IOException{
+        CAgreementsGroups agreementsGroups = cAgreementsGroupsService.lowGroup(idAg);
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(agreementsGroups),HttpStatus.OK);
     }
     
 }
