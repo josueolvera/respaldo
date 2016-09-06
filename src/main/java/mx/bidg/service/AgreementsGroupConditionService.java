cc9ffbbdc23f2bf03d1d03d5c6d246a733632e6e
josueolvera <josue.olvera.r@gmail.com>
2016-09-06 18:04:11 -0500

    Agrega servicio de seteo de tabulador y comision a una lista de Comisiones en AgreementsGroupConditionService

---
+++ src/main/java/mx/bidg/service/AgreementsGroupConditionService.java
@@ -0,0 +1,19 @@
+package mx.bidg.service;
+
+import mx.bidg.model.AgreementsGroupCondition;
+import mx.bidg.model.CommissionAmountGroup;
+
+import java.util.List;
+
+/**
+ * Created by josueolvera on 2/09/16.
+ */
+public interface AgreementsGroupConditionService {
+    AgreementsGroupCondition save(AgreementsGroupCondition agreementsGroupCondition);
+    AgreementsGroupCondition update (AgreementsGroupCondition agreementsGroupCondition);
+    AgreementsGroupCondition findById(Integer idGroupCondition);
+    boolean delete (AgreementsGroupCondition agreementsGroupCondition);
+    List<AgreementsGroupCondition> findAll();
+    List<CommissionAmountGroup> setTabulator (List <AgreementsGroupCondition> agreementsGroupConditionList);
+    List<AgreementsGroupCondition> conditions(Integer idAg);
+}
---
+++ src/main/java/mx/bidg/service/impl/AgreementsGroupConditionServiceImpl.java
@@ -0,0 +1,79 @@
+package mx.bidg.service.impl;
+
+import mx.bidg.dao.AgreementsGroupConditionDao;
+import mx.bidg.dao.CommissionAmountGroupDao;
+import mx.bidg.model.AgreementsGroupCondition;
+import mx.bidg.model.CommissionAmountGroup;
+import mx.bidg.service.AgreementsGroupConditionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Created by josueolvera on 2/09/16.
+ */
+@Service
+@Transactional
+public class AgreementsGroupConditionServiceImpl implements AgreementsGroupConditionService {
+
+    @Autowired
+    AgreementsGroupConditionDao agreementsGroupConditionDao;
+
+    @Autowired
+    CommissionAmountGroupDao commissionAmountGroupDao;
+
+    @Override
+    public AgreementsGroupCondition save(AgreementsGroupCondition agreementsGroupCondition) {
+        return agreementsGroupConditionDao.save(agreementsGroupCondition);
+    }
+
+    @Override
+    public AgreementsGroupCondition update(AgreementsGroupCondition agreementsGroupCondition) {
+        return agreementsGroupConditionDao.update(agreementsGroupCondition);
+    }
+
+    @Override
+    public AgreementsGroupCondition findById(Integer idGroupCondition) {
+        return agreementsGroupConditionDao.findById(idGroupCondition);
+    }
+
+    @Override
+    public boolean delete(AgreementsGroupCondition agreementsGroupCondition) {
+        agreementsGroupConditionDao.delete(agreementsGroupCondition);
+        return true;
+    }
+
+    @Override
+    public List<AgreementsGroupCondition> findAll() {
+        return agreementsGroupConditionDao.findAll();
+    }
+
+    @Override
+    public List<CommissionAmountGroup> setTabulator(List<AgreementsGroupCondition> agreementsGroupConditionList) {
+        int conditionNumber = agreementsGroupConditionList.size();
+
+        for (AgreementsGroupCondition aGC: agreementsGroupConditionList){
+                AgreementsGroupCondition groupCondition = agreementsGroupConditionDao.getTabulator(aGC.getOrder(),aGC);
+
+                List<CommissionAmountGroup> amountGroups = commissionAmountGroupDao.getComissionsByConditon(groupCondition);
+
+                for (CommissionAmountGroup cAGroup :  amountGroups){
+                    cAGroup.setTabulator(groupCondition.getTabulator());
+                    BigDecimal divisor = new BigDecimal(100);
+                    BigDecimal comission = groupCondition.getTabulator().divide(divisor);
+                    cAGroup.setCommission(cAGroup.getAmount().multiply(comission));
+                    commissionAmountGroupDao.update(cAGroup);
+                }
+        }
+
+        return commissionAmountGroupDao.findAll();
+    }
+
+    @Override
+    public List<AgreementsGroupCondition> conditions(Integer idAg) {
+        return agreementsGroupConditionDao.conditionList(idAg);
+    }
+}
