3c4693907ba874bfca93f552b828595eff46fccf
Gerardo8 <gerardo.lopezre@gmail.com>
2016-09-09 13:57:54 -0500

    Agrega clase CCostCenterDao

---
+++ src/main/java/mx/bidg/dao/CCostCenterDao.java
@@ -0,0 +1,10 @@
+package mx.bidg.dao;
+
+import mx.bidg.model.CContractType;
+import mx.bidg.model.CCostCenter;
+
+/**
+ * Created by gerardo8 on 09/09/16.
+ */
+public interface CCostCenterDao extends InterfaceDao<CCostCenter> {
+}
