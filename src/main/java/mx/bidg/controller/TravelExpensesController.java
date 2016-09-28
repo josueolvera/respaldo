2dd14d3046f6ab010b84d0781e7cf0936d68485c
Gerardo8 <gerardo.lopezre@gmail.com>
2016-09-28 13:46:24 -0500

    Agrega controlador getTravelExpenses

--- src/main/java/mx/bidg/controller/TravelExpensesController.java
+++ src/main/java/mx/bidg/controller/TravelExpensesController.java
@@ -12,10 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -45,4 +42,10 @@ public class TravelExpensesController {
         requestTravelExpenseEmailNotificationService.sendEmailToAdminAux(travelExpense);
         return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(travelExpense), HttpStatus.OK);
     }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> getTravelExpenses(@RequestParam(name = "user", required = false) Integer idUser) throws Exception {
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(travelExpensesService.getTravelExpenses(idUser)), HttpStatus.OK);
+    }
+
 }
