f95a2adacfe4d6ff313c1e021bcf5c28326ae064
josueolvera <josue.olvera.r@gmail.com>
2016-10-10 09:34:37 -0500

    Se agrega CDateCalculationDao con su respectiva implementacion

---
+++ src/main/java/mx/bidg/dao/CDateCalculationDao.java
@@ -0,0 +1,9 @@
+package mx.bidg.dao;
+
+import mx.bidg.model.CDateCalculation;
+
+/**
+ * Created by josueolvera on 6/10/16.
+ */
+public interface CDateCalculationDao extends InterfaceDao<CDateCalculation>{
+}
---
+++ src/main/java/mx/bidg/dao/impl/CDateCalculationDaoImpl.java
@@ -0,0 +1,43 @@
+package mx.bidg.dao.impl;
+
+import mx.bidg.dao.AbstractDao;
+import mx.bidg.dao.CDateCalculationDao;
+import mx.bidg.model.CDateCalculation;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by josueolvera on 6/10/16.
+ */
+@Repository
+public class CDateCalculationDaoImpl extends AbstractDao<Integer, CDateCalculation> implements CDateCalculationDao {
+
+    @Override
+    public CDateCalculation save(CDateCalculation entity) {
+        persist(entity);
+        return entity;
+    }
+
+    @Override
+    public CDateCalculation findById(int id) {
+        return getByKey(id);
+    }
+
+    @Override
+    public List<CDateCalculation> findAll() {
+        return createEntityCriteria().list();
+    }
+
+    @Override
+    public CDateCalculation update(CDateCalculation entity) {
+        modify(entity);
+        return entity;
+    }
+
+    @Override
+    public boolean delete(CDateCalculation entity) {
+        remove(entity);
+        return true;
+    }
+}
