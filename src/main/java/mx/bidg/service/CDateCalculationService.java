9fd9bda0f76a12b1a220110bab39032658043237
josueolvera <josue.olvera.r@gmail.com>
2016-10-10 09:35:17 -0500

    Se agrega CDateCalculationService con su respectiva implementacion

---
+++ src/main/java/mx/bidg/service/CDateCalculationService.java
@@ -0,0 +1,16 @@
+package mx.bidg.service;
+
+import mx.bidg.model.CDateCalculation;
+
+import java.util.List;
+
+/**
+ * Created by josueolvera on 6/10/16.
+ */
+public interface CDateCalculationService {
+    CDateCalculation save(CDateCalculation cDateCalculation);
+    CDateCalculation update(CDateCalculation cDateCalculation);
+    CDateCalculation findById(Integer idDateCalculation);
+    List<CDateCalculation> findAll ();
+    boolean delete (CDateCalculation cDateCalculation);
+}
---
+++ src/main/java/mx/bidg/service/impl/CDateCalculationServiceImpl.java
@@ -0,0 +1,47 @@
+package mx.bidg.service.impl;
+
+import mx.bidg.dao.CDateCalculationDao;
+import mx.bidg.model.CDateCalculation;
+import mx.bidg.service.CDateCalculationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by josueolvera on 6/10/16.
+ */
+@Service
+@Transactional
+public class CDateCalculationServiceImpl implements CDateCalculationService {
+
+    @Autowired
+    CDateCalculationDao cDateCalculationDao;
+
+    @Override
+    public CDateCalculation save(CDateCalculation cDateCalculation) {
+        return cDateCalculationDao.save(cDateCalculation);
+    }
+
+    @Override
+    public CDateCalculation update(CDateCalculation cDateCalculation) {
+        return cDateCalculationDao.update(cDateCalculation);
+    }
+
+    @Override
+    public CDateCalculation findById(Integer idDateCalculation) {
+        return cDateCalculationDao.findById(idDateCalculation);
+    }
+
+    @Override
+    public List<CDateCalculation> findAll() {
+        return cDateCalculationDao.findAll();
+    }
+
+    @Override
+    public boolean delete(CDateCalculation cDateCalculation) {
+        cDateCalculationDao.delete(cDateCalculation);
+        return true;
+    }
+}
