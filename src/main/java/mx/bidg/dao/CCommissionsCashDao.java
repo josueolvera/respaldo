f3c5b26c771834c4824c38bd1ad1f416a9e95306
yair <yairing23@outlook.com>
2016-11-15 18:47:25 -0600

    Se creo CCommissionsCashDao y CCommissionsCashDaoImpl

---
+++ src/main/java/mx/bidg/dao/CCommissionsCashDao.java
@@ -0,0 +1,9 @@
+package mx.bidg.dao;
+
+import mx.bidg.model.CCommissionsCash;
+
+/**
+ * Created by PC_YAIR on 15/11/2016.
+ */
+public interface CCommissionsCashDao extends InterfaceDao<CCommissionsCash> {
+}
---
+++ src/main/java/mx/bidg/dao/impl/CCommissionsCashDaoImpl.java
@@ -0,0 +1,42 @@
+package mx.bidg.dao.impl;
+
+import mx.bidg.dao.AbstractDao;
+import mx.bidg.dao.CCommissionsCashDao;
+import mx.bidg.model.CCommissionsCash;
+
+import java.util.List;
+
+/**
+ * Created by PC_YAIR on 15/11/2016.
+ */
+public class CCommissionsCashDaoImpl extends AbstractDao<Integer, CCommissionsCash> implements CCommissionsCashDao  {
+    @Override
+    public CCommissionsCash save(CCommissionsCash entity) {
+        persist(entity);
+        return entity;
+
+    }
+
+    @Override
+    public CCommissionsCash findById(int id) {
+        return getByKey(id);
+    }
+
+    @Override
+    public List<CCommissionsCash> findAll() {
+        return createEntityCriteria().list();
+    }
+
+    @Override
+    public CCommissionsCash update(CCommissionsCash entity) {
+
+        modify(entity);
+        return entity;
+    }
+
+    @Override
+    public boolean delete(CCommissionsCash entity) {
+        delete(entity);
+        return true;
+    }
+}
