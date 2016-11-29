d5617c197ca157c41d002852d47361940d3710fe
JosueOlvera <josue.olvera.r@gmail.com>
2016-11-29 09:03:55 -0600

    Se agrega busqueda por dwEnterprises y fecha de aplicacion y tambn se agrega metodo de suma de total de rhmas para reporte de Distribucion

--- src/main/java/mx/bidg/dao/OutsourcingDao.java
+++ src/main/java/mx/bidg/dao/OutsourcingDao.java
@@ -1,12 +1,16 @@
 package mx.bidg.dao;
 
+import mx.bidg.model.DwEnterprises;
 import mx.bidg.model.Outsourcing;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Created by gerardo8 on 16/05/16.
  */
 public interface OutsourcingDao extends InterfaceDao<Outsourcing> {
     Outsourcing finfByidEmployee(int idEmployee,LocalDateTime applicationDate);
+    List<Outsourcing> findByDwEnterprise(List<DwEnterprises> dwEnterprisesList, LocalDateTime applicatioDateStart, LocalDateTime applicationDateEnd);
+    Object findSumRhmasByDwEnterprise(List<DwEnterprises> dwEnterprisesList, LocalDateTime applicatioDateStart, LocalDateTime applicationDateEnd);
 }
