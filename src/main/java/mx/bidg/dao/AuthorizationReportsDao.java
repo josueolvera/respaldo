8ff992ce29c950c0b8c9a8ffa8668d836ca3a94f
JosueOlvera <josue.olvera.r@gmail.com>
2016-12-21 16:16:43 -0600

    Agrega busqueda de por idSqlQuery y busqueda por role y query y por idQuery y Autorizado en AuthorizationReportsDao

--- src/main/java/mx/bidg/dao/AuthorizationReportsDao.java
+++ src/main/java/mx/bidg/dao/AuthorizationReportsDao.java
@@ -2,8 +2,13 @@ package mx.bidg.dao;
 
 import mx.bidg.model.AuthorizationReports;
 
+import java.util.List;
+
 /**
  * Created by josue on 02/12/2016.
  */
 public interface AuthorizationReportsDao extends InterfaceDao<AuthorizationReports> {
+    List<AuthorizationReports> findByIdSqlQuery(Integer idSqlQuery);
+    AuthorizationReports findByIdRoleAndIdQuery(Integer idRole, Integer idQuery);
+    List<AuthorizationReports> findByidQueryAndAuthorized(Integer idQuery);
 }
--- src/main/java/mx/bidg/dao/impl/AuthorizationReportsDaoImpl.java
+++ src/main/java/mx/bidg/dao/impl/AuthorizationReportsDaoImpl.java
@@ -3,6 +3,8 @@ package mx.bidg.dao.impl;
 import mx.bidg.dao.AbstractDao;
 import mx.bidg.dao.AuthorizationReportsDao;
 import mx.bidg.model.AuthorizationReports;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
@@ -40,5 +42,32 @@ public class AuthorizationReportsDaoImpl extends AbstractDao<Integer, Authorizat
     public boolean delete(AuthorizationReports entity) {
         remove(entity);
         return true;
+    }
+
+    @Override
+    public List<AuthorizationReports> findByIdSqlQuery(Integer idSqlQuery) {
+        Criteria criteria = createEntityCriteria();
+
+        return criteria.add(Restrictions.eq("idQuery", idSqlQuery)).list();
+    }
+
+    @Override
+    public AuthorizationReports findByIdRoleAndIdQuery(Integer idRole, Integer idQuery) {
+        Criteria criteria = createEntityCriteria();
+
+        return (AuthorizationReports) criteria
+                .add(Restrictions.eq("idRole", idRole))
+                .add(Restrictions.eq("idQuery", idQuery))
+                .uniqueResult();
+    }
+
+    @Override
+    public List<AuthorizationReports> findByidQueryAndAuthorized(Integer idQuery) {
+        Criteria criteria = createEntityCriteria();
+
+        return criteria
+                .add(Restrictions.eq("idQuery", idQuery))
+                .add(Restrictions.eq("authorization", 1))
+                .list();
     }
 }
