e87bea5d32265a3fb4247018a29285add6f4b5b7
JosueOlvera <josue.olvera.r@gmail.com>
2016-12-21 16:19:04 -0600

    Agrega metodo de busqueda por nombre y reportes por corporativo y outsourcing

--- src/main/java/mx/bidg/dao/CalculationReportDao.java
+++ src/main/java/mx/bidg/dao/CalculationReportDao.java
@@ -2,8 +2,15 @@ package mx.bidg.dao;
 
 import mx.bidg.model.CalculationReport;
 
+import java.util.List;
+
 /**
  * Created by josue on 02/12/2016.
  */
 public interface CalculationReportDao extends InterfaceDao<CalculationReport> {
+    CalculationReport findByName (String fileName);
+    List<CalculationReport> findReportsGeneratedAndNotSended();
+    List<CalculationReport> findReportsGeneratedAndSendedNotAuthorized();
+    List<CalculationReport> findAllReportsAuthorizedCorporate();
+    List<CalculationReport> findAllReportsAuthorizedOutsourcing();
 }
--- src/main/java/mx/bidg/dao/impl/CalculationReportDaoImpl.java
+++ src/main/java/mx/bidg/dao/impl/CalculationReportDaoImpl.java
@@ -3,6 +3,9 @@ package mx.bidg.dao.impl;
 import mx.bidg.dao.AbstractDao;
 import mx.bidg.dao.CalculationReportDao;
 import mx.bidg.model.CalculationReport;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -39,5 +42,60 @@ public class CalculationReportDaoImpl extends AbstractDao<Integer, CalculationRe
     public boolean delete(CalculationReport entity) {
         remove(entity);
         return true;
+    }
+
+    @Override
+    public CalculationReport findByName(String fileName) {
+        Criteria criteria = createEntityCriteria();
+
+        return (CalculationReport) criteria.add(Restrictions.eq("fileName", fileName)).uniqueResult();
+    }
+
+    @Override
+    public List<CalculationReport> findReportsGeneratedAndNotSended() {
+        Criteria criteria = createEntityCriteria();
+
+        return criteria
+                .add(Restrictions.eq("status",0))
+                .add(Restrictions.eq("send",0))
+                .add(Restrictions.eq("showWindow",1))
+                .list()
+                ;
+    }
+
+    @Override
+    public List<CalculationReport> findReportsGeneratedAndSendedNotAuthorized() {
+        Criteria criteria = createEntityCriteria();
+
+        return criteria
+                .add(Restrictions.eq("status",0))
+                .add(Restrictions.eq("send",1))
+                .add(Restrictions.eq("showWindow",1))
+                .list()
+                ;
+    }
+
+    @Override
+    public List<CalculationReport> findAllReportsAuthorizedCorporate() {
+        Criteria criteria = createEntityCriteria();
+
+        return criteria
+                .add(Restrictions.eq("status",1))
+                .add(Restrictions.eq("send",1))
+                .add(Restrictions.eq("showWindow",1))
+                .list()
+                ;
+    }
+
+    @Override
+    public List<CalculationReport> findAllReportsAuthorizedOutsourcing() {
+        Criteria criteria = createEntityCriteria();
+
+        return criteria
+                .add(Restrictions.eq("status",1))
+                .add(Restrictions.eq("send",1))
+                .add(Restrictions.eq("showWindow",2))
+                .list()
+                ;
     }
 }
