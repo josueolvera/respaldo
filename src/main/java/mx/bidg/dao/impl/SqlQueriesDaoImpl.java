0e7ad9f0074b054fcab87cdbbeda5febfc16c1f9
Kevin <ingkevinsjm@hotmail.com>
2016-12-30 12:08:25 -0600

    "

--- src/main/java/mx/bidg/dao/impl/SqlQueriesDaoImpl.java
+++ src/main/java/mx/bidg/dao/impl/SqlQueriesDaoImpl.java
@@ -101,10 +101,9 @@ public class SqlQueriesDaoImpl extends AbstractDao<Integer, SqlQueries> implemen
                 .setParameter("fechaSemanal",week4Init);
         return sqlQuery.list();
     }
-    //Pendiente
+
     @Override
     public List<SqlQueries> findByCalculateReport() {
         return (List<SqlQueries>) createEntityCriteria().add(Restrictions.eq("calculate",2)).list();
     }
-    
 }
