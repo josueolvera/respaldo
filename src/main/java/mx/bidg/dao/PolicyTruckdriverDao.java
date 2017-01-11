b8baff4182e9b0cff1962f13a70ca9e8255fdb2f
yair <yairing23@outlook.com>
2017-01-11 16:43:47 -0600

    Se agregó la lista findByDate en el PolicyTruckdriverDao

--- src/main/java/mx/bidg/dao/PolicyTruckdriverDao.java
+++ src/main/java/mx/bidg/dao/PolicyTruckdriverDao.java
@@ -1,6 +1,7 @@
 package mx.bidg.dao;
 
 import mx.bidg.model.PolicyTruckdriver;
+import org.exolab.castor.types.Date;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -10,6 +11,6 @@ import java.util.List;
  * Created by PC_YAIR on 11/01/2017.
  */
 public interface PolicyTruckdriverDao extends InterfaceDao<PolicyTruckdriver> {
-
+    List findDate (String starDate);
 
 }
