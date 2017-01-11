1e14c6786985d84ef13611925bd30f6c424db49b
yair <yairing23@outlook.com>
2017-01-11 13:33:46 -0600

    Se corriguio el SinisterTruckdriverDao

--- src/main/java/mx/bidg/dao/SinisterTruckdriverDao.java
+++ src/main/java/mx/bidg/dao/SinisterTruckdriverDao.java
@@ -1,7 +1,9 @@
 package mx.bidg.dao;
 
+import mx.bidg.model.SinisterTruckdriver;
+
 /**
  * Created by PC_YAIR on 11/01/2017.
  */
-public interface SinisterTruckdriverDao {
+public interface SinisterTruckdriverDao extends InterfaceDao<SinisterTruckdriver> {
 }
