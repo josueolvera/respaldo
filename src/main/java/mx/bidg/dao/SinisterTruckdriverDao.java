82f7f5325605575f0f91b066fa03baaf942aeffc
Kevin <ingkevinsjm@hotmail.com>
2017-01-11 18:46:43 -0600

    Se agrego metodo findByDateStart

--- src/main/java/mx/bidg/dao/SinisterTruckdriverDao.java
+++ src/main/java/mx/bidg/dao/SinisterTruckdriverDao.java
@@ -2,8 +2,12 @@ package mx.bidg.dao;
 
 import mx.bidg.model.SinisterTruckdriver;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by PC_YAIR on 11/01/2017.
  */
 public interface SinisterTruckdriverDao extends InterfaceDao<SinisterTruckdriver> {
+    List<SinisterTruckdriver>findByDateStart(Integer idTipeAssistance,String startDate);
 }
--- src/main/java/mx/bidg/dao/impl/SinisterTruckdriverDaoImpl.java
+++ src/main/java/mx/bidg/dao/impl/SinisterTruckdriverDaoImpl.java
@@ -3,12 +3,17 @@ package mx.bidg.dao.impl;
 import mx.bidg.dao.AbstractDao;
 import mx.bidg.dao.SinisterTruckdriverDao;
 import mx.bidg.model.SinisterTruckdriver;
+import org.apache.xmlbeans.impl.xb.xsdschema.RestrictionDocument;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by PC_YAIR on 11/01/2017.
  */
+@Repository
 public class SinisterTruckdriverDaoImpl extends AbstractDao<Integer,SinisterTruckdriver> implements SinisterTruckdriverDao {
     @Override
     public SinisterTruckdriver save(SinisterTruckdriver entity) {
@@ -36,5 +41,10 @@ public class SinisterTruckdriverDaoImpl extends AbstractDao<Integer,SinisterTruc
     public boolean delete(SinisterTruckdriver entity) {
         delete(entity);
         return true;
+    }
+
+    @Override
+    public List<SinisterTruckdriver> findByDateStart(Integer idTipeAssistance, String startDate) {
+        return createEntityCriteria().add(Restrictions.eq("idTipeAssistance",idTipeAssistance)).add(Restrictions.eq("startDate",startDate)).list();
     }
 }
