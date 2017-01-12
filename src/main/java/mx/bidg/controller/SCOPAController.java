83ad3f92f0685776318db310dddb309737abbcc4
Kevin <ingkevinsjm@hotmail.com>
2017-01-11 18:42:50 -0600

    Se genero el controller para el sistema SCOPA

---
+++ src/main/java/mx/bidg/controller/SCOPAController.java
@@ -0,0 +1,21 @@
+package mx.bidg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Kevin Salvador on 10/01/2017.
+ */
+@Controller
+@RequestMapping("/scopa")
+public class SCOPAController {
+
+    @RequestMapping(value = "/truck-driver", method = RequestMethod.GET,produces = "text/html;charset=UTF-8")
+    public ModelAndView truckdriver(){
+        ModelAndView model= new ModelAndView();
+        model.setViewName("truckDriver");
+        return model;
+    }
+}
