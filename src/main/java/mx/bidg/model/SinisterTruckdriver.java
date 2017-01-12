8ae2cac78656d0a0df7c96ddcd30e1e34ff334a2
JosueOlvera <josue.olvera.r@gmail.com>
2017-01-12 16:18:00 -0600

    Se corrige clase para su correcto funcionamiento

--- src/main/java/mx/bidg/model/SinisterTruckdriver.java
+++ src/main/java/mx/bidg/model/SinisterTruckdriver.java
@@ -8,10 +8,13 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import mx.bidg.config.JsonViews;
+import mx.bidg.utils.DateConverter;
 import mx.bidg.utils.DateTimeConverter;
+import mx.bidg.utils.TimeConverter;
 import org.hibernate.annotations.DynamicUpdate;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Date;
@@ -39,32 +42,31 @@ public class SinisterTruckdriver implements Serializable {
 
     @Column(name = "NUM_LICENSE_PLATE")
     @JsonView(JsonViews.Root.class)
-    private Integer numLicensePlate;
+    private String numLicensePlate;
 
     @Basic(optional = false)
     @NotNull
     @Column(name = "D_START_VALIDITY")
     @JsonView(JsonViews.Root.class)
-    @Convert(converter = DateTimeConverter.class)
-    private LocalDateTime dStartValidity;
+    @Convert(converter = DateConverter.class)
+    private LocalDate dStartValidity;
 
     @Basic(optional = false)
     @NotNull
     @Column(name = "D_END_VALIDITY")
     @JsonView(JsonViews.Root.class)
-    @Convert(converter = DateTimeConverter.class)
-    private LocalDateTime dEndValidity;
+    @Convert(converter = DateConverter.class)
+    private LocalDate dEndValidity;
 
     @Basic(optional = false)
     @NotNull
     @Column(name = "D_SINISTER")
-    @Temporal(TemporalType.TIMESTAMP)
     @JsonView(JsonViews.Root.class)
-    @Convert(converter = DateTimeConverter.class)
-    private LocalDateTime dSinister;
+    @Convert(converter = DateConverter.class)
+    private LocalDate dSinister;
 
     @Column(name = "H_SINISTER")
-    @Temporal(TemporalType.TIME)
+    @Convert(converter = TimeConverter.class)
     @JsonView(JsonViews.Root.class)
     private LocalTime hSinister;
 
@@ -73,10 +75,20 @@ public class SinisterTruckdriver implements Serializable {
     @JsonView(JsonViews.Root.class)
     private String locationSinister;
 
+    @Column(name = "ID_TYPE_ASSISTANCE", insertable = false, updatable = false)
+    @JsonView(JsonViews.Root.class)
+    private int idTypeAssistance;
+
     @JoinColumn(name = "ID_TYPE_ASSISTANCE", referencedColumnName = "ID_TYPE_ASSISTANCE")
     @ManyToOne
     @JsonView(JsonViews.Embedded.class)
-    private CTypeAssistance idTypeAssistance;
+    private CTypeAssistance cTypeAssistance;
+
+    @Basic(optional = false)
+    @Column(name = "CREATION_DATE")
+    @Convert(converter = DateTimeConverter.class)
+    @JsonView(JsonViews.Root.class)
+    private LocalDateTime creationDate;
 
     public SinisterTruckdriver() {
     }
@@ -85,7 +97,7 @@ public class SinisterTruckdriver implements Serializable {
         this.idSinister = idSinister;
     }
 
-    public SinisterTruckdriver(Integer idSinister, LocalDateTime dStartValidity, LocalDateTime dEndValidity, LocalDateTime dSinister) {
+    public SinisterTruckdriver(Integer idSinister, LocalDate dStartValidity, LocalDate dEndValidity, LocalDate dSinister) {
         this.idSinister = idSinister;
         this.dStartValidity = dStartValidity;
         this.dEndValidity = dEndValidity;
@@ -100,44 +112,12 @@ public class SinisterTruckdriver implements Serializable {
         this.idSinister = idSinister;
     }
 
-    public Integer getNumLicensePlate() {
+    public String getNumLicensePlate() {
         return numLicensePlate;
     }
 
-    public void setNumLicensePlate(Integer numLicensePlate) {
+    public void setNumLicensePlate(String numLicensePlate) {
         this.numLicensePlate = numLicensePlate;
-    }
-
-    public LocalDateTime getDStartValidity() {
-        return dStartValidity;
-    }
-
-    public void setDStartValidity(LocalDateTime dStartValidity) {
-        this.dStartValidity = dStartValidity;
-    }
-
-    public LocalDateTime getDEndValidity() {
-        return dEndValidity;
-    }
-
-    public void setDEndValidity(LocalDateTime dEndValidity) {
-        this.dEndValidity = dEndValidity;
-    }
-
-    public LocalDateTime getDSinister() {
-        return dSinister;
-    }
-
-    public void setDSinister(LocalDateTime dSinister) {
-        this.dSinister = dSinister;
-    }
-
-    public LocalTime getHSinister() {
-        return hSinister;
-    }
-
-    public void setHSinister(LocalTime hSinister) {
-        this.hSinister = hSinister;
     }
 
     public String getLocationSinister() {
@@ -148,12 +128,60 @@ public class SinisterTruckdriver implements Serializable {
         this.locationSinister = locationSinister;
     }
 
-    public CTypeAssistance getIdTypeAssistance() {
+    public int getIdTypeAssistance() {
         return idTypeAssistance;
     }
 
-    public void setIdTypeAssistance(CTypeAssistance idTypeAssistance) {
+    public void setIdTypeAssistance(int idTypeAssistance) {
         this.idTypeAssistance = idTypeAssistance;
+    }
+
+    public CTypeAssistance getcTypeAssistance() {
+        return cTypeAssistance;
+    }
+
+    public void setcTypeAssistance(CTypeAssistance cTypeAssistance) {
+        this.cTypeAssistance = cTypeAssistance;
+    }
+
+    public LocalDate getdStartValidity() {
+        return dStartValidity;
+    }
+
+    public void setdStartValidity(LocalDate dStartValidity) {
+        this.dStartValidity = dStartValidity;
+    }
+
+    public LocalDate getdEndValidity() {
+        return dEndValidity;
+    }
+
+    public void setdEndValidity(LocalDate dEndValidity) {
+        this.dEndValidity = dEndValidity;
+    }
+
+    public LocalDate getdSinister() {
+        return dSinister;
+    }
+
+    public void setdSinister(LocalDate dSinister) {
+        this.dSinister = dSinister;
+    }
+
+    public LocalTime gethSinister() {
+        return hSinister;
+    }
+
+    public void sethSinister(LocalTime hSinister) {
+        this.hSinister = hSinister;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
     }
 
     @Override
