9e197ee308073b34c6b9eedf79f2354f6f817bdf
JosueOlvera <josue.olvera.r@gmail.com>
2017-01-12 16:10:50 -0600

    Se agrega CTypeSecureDao y su respectiva implementacion

---
+++ src/main/java/mx/bidg/dao/CTypeSecureDao.java
@@ -0,0 +1,9 @@
+package mx.bidg.dao;
+
+import mx.bidg.model.CTypeSecure;
+
+/**
+ * Created by Desarrollador on 12/01/2017.
+ */
+public interface CTypeSecureDao extends InterfaceDao<CTypeSecure> {
+}
---
+++ src/main/java/mx/bidg/dao/impl/CTypeSecureDaoImpl.java
@@ -0,0 +1,43 @@
+package mx.bidg.dao.impl;
+
+import mx.bidg.dao.AbstractDao;
+import mx.bidg.dao.CTypeSecureDao;
+import mx.bidg.model.CTypeSecure;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by Desarrollador on 12/01/2017.
+ */
+@Repository
+public class CTypeSecureDaoImpl extends AbstractDao<Integer, CTypeSecure> implements CTypeSecureDao {
+
+    @Override
+    public CTypeSecure save(CTypeSecure entity) {
+        persist(entity);
+        return entity;
+    }
+
+    @Override
+    public CTypeSecure findById(int id) {
+        return getByKey(id);
+    }
+
+    @Override
+    public List<CTypeSecure> findAll() {
+        return createEntityCriteria().list();
+    }
+
+    @Override
+    public CTypeSecure update(CTypeSecure entity) {
+        modify(entity);
+        return entity;
+    }
+
+    @Override
+    public boolean delete(CTypeSecure entity) {
+        remove(entity);
+        return true;
+    }
+}
