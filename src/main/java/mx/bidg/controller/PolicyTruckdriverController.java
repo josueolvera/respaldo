e4fd888d63f9db54071ac9d43a5d739414f190ca
JosueOlvera <josue.olvera.r@gmail.com>
2017-03-21 18:47:19 -0600

    Se agrega controller de descarga por medio de ftp y busqueda de archivo en el servidor

--- src/main/java/mx/bidg/controller/PolicyTruckdriverController.java
+++ src/main/java/mx/bidg/controller/PolicyTruckdriverController.java
@@ -5,19 +5,23 @@ import mx.bidg.config.JsonViews;
 import mx.bidg.model.PolicyTruckdriver;
 import mx.bidg.service.CTypeSecureService;
 import mx.bidg.service.PolicyTruckdriverService;
+import mx.bidg.utils.FtpConnect;
 import org.apache.http.HttpStatus;
 import org.apache.xml.serialize.Method;
 import org.exolab.castor.types.Date;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -32,51 +36,71 @@ public class PolicyTruckdriverController {
     private PolicyTruckdriverService policyTruckdriverService;
 
     @Autowired
-    private CTypeSecureService cTypeSecureService;
+    private ObjectMapper mapper;
 
     @Autowired
-    private ObjectMapper mapper;
+    private Environment env;
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<String> findAll() throws Exception {
         return ResponseEntity.ok(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(policyTruckdriverService.findAll()));
     }
 
-
-    @RequestMapping(value = "/get-by-date", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<String> findByDate(@RequestParam(name = "startDate", required = true) String startDate) throws Exception {
-
-        LocalDate dStartValidity = (startDate == null || startDate.equals("")) ? null :
-                LocalDate.parse(startDate, DateTimeFormatter.ISO_LOCAL_DATE);
-
-        List<PolicyTruckdriver> policyTruckdriverList = policyTruckdriverService.findByDStartValidity(dStartValidity);
-
-        if(!policyTruckdriverList.isEmpty()){
-            for (PolicyTruckdriver policyTruckdriver : policyTruckdriverList){
-                BigDecimal priceSale = null;
-                BigDecimal cost = null;
-                BigDecimal total;
-                if (policyTruckdriver.getcTypeSecure() != null){
-                    if (policyTruckdriver.getcTypeSecure().getPriceSale() != null){
-                        policyTruckdriver.setPriceSale(policyTruckdriver.getcTypeSecure().getPriceSale());
-                        priceSale = policyTruckdriver.getcTypeSecure().getPriceSale();
-                    }
-                    if (policyTruckdriver.getcTypeSecure().getCommission() != null){
-                        if (priceSale != null){
-                            cost = priceSale.multiply(policyTruckdriver.getcTypeSecure().getCommission());
-                        }
-                        policyTruckdriver.setCost(cost);
-                    }
-                    if (cost != null){
-                        total = priceSale.subtract(cost);
-                        policyTruckdriver.setTotal(total);
-                    }
-                }
-            }
+//    @Scheduled(cron = "0 15 0 * * ?")
+    @RequestMapping(value = "/csv", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public void readCSV()throws IOException{
+        Calendar fecha = Calendar.getInstance();
+        String name = "EMIASEG_CANALDEPAGO_";
+        fecha.add(Calendar.DAY_OF_YEAR,-1);
+        int año = fecha.get(Calendar.YEAR);
+        int mes = fecha.get(Calendar.MONTH) + 1;
+        int dia = fecha.get(Calendar.DAY_OF_MONTH);
+        if (dia < 10){
+            name = name+"0"+dia;
+        }else{
+            name = name+dia;
         }
-
-        return ResponseEntity.ok( mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(policyTruckdriverList));
+        if (mes < 10){
+            name = name+"0"+mes;
+        }else {
+            name = name + mes;
+        }
+        name = name+año+".csv";
+        File file = new File(env.getRequiredProperty("policy_truckDriver.documents_dir")+name);
+        if (file.exists()){
+            policyTruckdriverService.readCsvPolicya(name);
+        }
     }
 
+    //    @Scheduled(cron = "0 15 0 * * ?")
+    @RequestMapping(value = "/ftp-download", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public void ftpDownload() throws Exception{
+        Calendar fecha = Calendar.getInstance();
+        String name = "EMIASEG_CANALDEPAGO_";
+        fecha.add(Calendar.DAY_OF_YEAR,-1);
+        int año = fecha.get(Calendar.YEAR);
+        int mes = fecha.get(Calendar.MONTH) + 1;
+        int dia = fecha.get(Calendar.DAY_OF_MONTH);
+        if (dia < 10){
+            name = name+"0"+dia;
+        }else{
+            name = name+dia;
+        }
+        if (mes < 10){
+            name = name+"0"+mes;
+        }else {
+            name = name + mes;
+        }
+        name = name+año+".csv";
 
+        File file = new File(env.getRequiredProperty("policy_truckDriver.documents_dir")+name);
+        if (!file.exists()){
+            FtpConnect ftpConnect = new FtpConnect();
+            ftpConnect.connect(env.getRequiredProperty("ftp_server_domain"), env.getRequiredProperty("ftp_server_user"), env.getRequiredProperty("ftp_server_password"));
+            ftpConnect.downloadFile(name,env.getRequiredProperty("policy_truckDriver.documents_dir")+name);
+            ftpConnect.disconnect();
+        }else {
+            System.out.println("ya existe");
+        }
+    }
 }
