2702f149e9ab24da2c7082a3224efa715a46248e
JosueOlvera <josue.olvera.r@gmail.com>
2017-03-21 18:35:35 -0600

    Se agrega busqueda por PaymentDate y numero de autorizacion en MrPayTruckDriverDao

--- src/main/java/mx/bidg/dao/MrPayTruckDriverDao.java
+++ src/main/java/mx/bidg/dao/MrPayTruckDriverDao.java
@@ -2,8 +2,15 @@ package mx.bidg.dao;
 
 import mx.bidg.model.MrPayTruckDriver;
 
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  * Created by Kevin Salvador on 19/01/2017.
  */
 public interface MrPayTruckDriverDao extends InterfaceDao<MrPayTruckDriver> {
+    MrPayTruckDriver findAuthorizationNumber(String authorizationNumber);
+    List<MrPayTruckDriver> findByPaymentDate(LocalDate paymentDate);
+    List<String> findNoAutizationByDStartValidity(LocalDate startDate, LocalDate endDate);
+    List<MrPayTruckDriver> findByPaymentDateBetween(LocalDate startDate, LocalDate endDate);
 }
--- src/main/java/mx/bidg/dao/impl/MrPayTruckDriverDaoImpl.java
+++ src/main/java/mx/bidg/dao/impl/MrPayTruckDriverDaoImpl.java
@@ -3,8 +3,12 @@ package mx.bidg.dao.impl;
 import mx.bidg.dao.AbstractDao;
 import mx.bidg.dao.MrPayTruckDriverDao;
 import mx.bidg.model.MrPayTruckDriver;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -38,5 +42,33 @@ public class MrPayTruckDriverDaoImpl extends AbstractDao<Integer,MrPayTruckDrive
     public boolean delete(MrPayTruckDriver entity) {
         remove(entity);
         return true;
+    }
+
+    @Override
+    public MrPayTruckDriver findAuthorizationNumber(String authorizationNumber) {
+        Criteria criteria = createEntityCriteria();
+        return (MrPayTruckDriver) criteria.add(Restrictions.eq("authorizationNumber",authorizationNumber)).uniqueResult();
+    }
+
+    @Override
+    public List<MrPayTruckDriver> findByPaymentDate(LocalDate paymentDate) {
+        Criteria criteria =  createEntityCriteria();
+        return criteria.add(Restrictions.eq("paymentDate", paymentDate)).list();
+    }
+
+    @Override
+    public List<String> findNoAutizationByDStartValidity(LocalDate startDate, LocalDate endDate) {
+        Criteria criteria = createEntityCriteria();
+
+        return criteria
+                .setProjection(Projections.property("authorizationNumber"))
+                .add(Restrictions.between("paymentDate",startDate, endDate))
+                .list();
+    }
+
+    @Override
+    public List<MrPayTruckDriver> findByPaymentDateBetween(LocalDate startDate, LocalDate endDate) {
+        Criteria criteria =  createEntityCriteria();
+        return criteria.add(Restrictions.between("paymentDate", startDate, endDate)).list();
     }
 }
