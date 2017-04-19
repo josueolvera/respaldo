dbb5929789380b5acd3f8da7b8fa6e7b5294e330
JosueOlvera <josue.olvera.r@gmail.com>
2017-04-19 13:26:41 -0500

    Se agrega clase CSubareas

---
+++ src/main/java/mx/bidg/model/CSubareas.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.bidg.model;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import mx.bidg.config.JsonViews;
+import mx.bidg.utils.DateTimeConverter;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author josue
+ */
+@Entity
+@DynamicUpdate
+@Table(name = "C_SUBAREAS")
+public class CSubareas implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID_SUBAREA")
+    @JsonView(JsonViews.Root.class)
+    private Integer idSubarea;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "SUBAREA_NAME")
+    @JsonView(JsonViews.Root.class)
+    private String subareaName;
+
+    @Size(max = 20)
+    @Column(name = "ACRONYM")
+    @JsonView(JsonViews.Root.class)
+    private String acronym;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "CREATION_DATE")
+    @Convert(converter = DateTimeConverter.class)
+    @JsonView(JsonViews.Root.class)
+    private Date creationDate;
+
+    @Size(max = 50)
+    @Column(name = "USERNAME")
+    @JsonView(JsonViews.Root.class)
+    private String username;
+
+    public CSubareas() {
+    }
+
+    public CSubareas(Integer idSubarea) {
+        this.idSubarea = idSubarea;
+    }
+
+    public CSubareas(Integer idSubarea, String subareaName, Date creationDate) {
+        this.idSubarea = idSubarea;
+        this.subareaName = subareaName;
+        this.creationDate = creationDate;
+    }
+
+    public Integer getIdSubarea() {
+        return idSubarea;
+    }
+
+    public void setIdSubarea(Integer idSubarea) {
+        this.idSubarea = idSubarea;
+    }
+
+    public String getSubareaName() {
+        return subareaName;
+    }
+
+    public void setSubareaName(String subareaName) {
+        this.subareaName = subareaName;
+    }
+
+    public String getAcronym() {
+        return acronym;
+    }
+
+    public void setAcronym(String acronym) {
+        this.acronym = acronym;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idSubarea != null ? idSubarea.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CSubareas)) {
+            return false;
+        }
+        CSubareas other = (CSubareas) object;
+        if ((this.idSubarea == null && other.idSubarea != null) || (this.idSubarea != null && !this.idSubarea.equals(other.idSubarea))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.bidg.model.CSubareas[ idSubarea=" + idSubarea + " ]";
+    }
+    
+}
