ebb1a1e9fa25961cdeb470d2717fc8a8013b3cae
JosueOlvera <josue.olvera.r@gmail.com>
2017-04-19 11:40:28 -0500

    Se agrega controller de persistencia de area

--- src/main/java/mx/bidg/controller/CAreasController.java
+++ src/main/java/mx/bidg/controller/CAreasController.java
@@ -5,9 +5,11 @@
  */
 package mx.bidg.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
@@ -15,16 +17,17 @@ import mx.bidg.config.JsonViews;
 import mx.bidg.model.CAreas;
 import mx.bidg.model.CGroups;
 import mx.bidg.model.DwEnterprises;
+import mx.bidg.model.Users;
 import mx.bidg.service.CAreasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import javax.xml.ws.Response;
 
 /**
  *
@@ -55,5 +58,23 @@ public class CAreasController {
     public ResponseEntity<String> getAreasWithRoles(@PathVariable Integer idArea)throws IOException{
         CAreas areas = cAreasService.findAreaWhitRole(idArea);
         return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(areas),HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> saveAreas (@RequestBody String data, HttpSession session) throws IOException{
+        Users user = (Users) session.getAttribute("user");
+
+        JsonNode node = mapper.readTree(data);
+
+        CAreas area = new CAreas();
+
+        area.setAreaName(node.get("areaName").asText());
+        area.setCreationDate(LocalDateTime.now());
+        area.setSaemFlag(0);
+        area.setUsername(user.getUsername());
+
+        cAreasService.save(area);
+
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(cAreasService.findAll()), HttpStatus.OK);
     }
 }
