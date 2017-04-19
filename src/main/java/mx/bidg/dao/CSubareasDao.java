55ed4cb3e40d8a5dea6ddaedcb68742f8ef15f80
JosueOlvera <josue.olvera.r@gmail.com>
2017-04-19 13:27:04 -0500

    Se agrega CSubareasDao con su implementacion

---
+++ src/main/java/mx/bidg/dao/CSubareasDao.java
@@ -0,0 +1,9 @@
+package mx.bidg.dao;
+
+import mx.bidg.model.CSubareas;
+
+/**
+ * Created by josue on 19/04/2017.
+ */
+public interface CSubareasDao extends InterfaceDao<CSubareas> {
+}
---
+++ src/main/java/mx/bidg/dao/impl/CSubareasDaoImpl.java
@@ -0,0 +1,43 @@
+package mx.bidg.dao.impl;
+
+import mx.bidg.dao.AbstractDao;
+import mx.bidg.dao.CSubareasDao;
+import mx.bidg.model.CSubareas;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by josue on 19/04/2017.
+ */
+@Repository
+public class CSubareasDaoImpl extends AbstractDao<Integer, CSubareas> implements CSubareasDao {
+
+    @Override
+    public CSubareas save(CSubareas entity) {
+        persist(entity);
+        return entity;
+    }
+
+    @Override
+    public CSubareas findById(int id) {
+        return getByKey(id);
+    }
+
+    @Override
+    public List<CSubareas> findAll() {
+        return createEntityCriteria().list();
+    }
+
+    @Override
+    public CSubareas update(CSubareas entity) {
+        modify(entity);
+        return entity;
+    }
+
+    @Override
+    public boolean delete(CSubareas entity) {
+        remove(entity);
+        return true;
+    }
+}
