020b8f337491d54f159bfd493be31f15936cb94e
Julio Cesar <jsantiago@bidg.mx>
2017-04-25 16:55:25 -0500

    Agrega controller de persistencia de zona en CZonasController

--- src/main/java/mx/bidg/controller/CZonasController.java
+++ src/main/java/mx/bidg/controller/CZonasController.java
@@ -1,18 +1,24 @@
 package mx.bidg.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import mx.bidg.config.JsonViews;
+import mx.bidg.model.CRegions;
 import mx.bidg.model.CZonas;
+import mx.bidg.model.Users;
 import mx.bidg.service.CZonasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -32,5 +38,21 @@ public class CZonasController {
     public ResponseEntity<String> findAll() throws IOException{
         List<CZonas> zonasList = zonasService.findAll();
         return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(zonasList), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/save-zonas", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> save(@RequestBody String data, HttpSession session)throws IOException{
+        JsonNode node = mapper.readTree(data);
+        Users user = (Users) session.getAttribute("user");
+        CZonas cZonas = new CZonas();
+        cZonas.setName(node.get("name").asText());
+        cZonas.setSaemFlag(0);
+        cZonas.setCreationDate(LocalDateTime.now());
+        cZonas.setUsername(user.getUsername());
+
+        zonasService.save(cZonas);
+
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(zonasService.findAll())
+                ,HttpStatus.OK);
     }
 }
