fd9dc752820821616438da3b6147a069e03f7c46
Julio Cesar <jsantiago@bidg.mx>
2017-04-26 16:54:36 -0500

    CSubareas modelo
    se agrego nuevos atributos en el modelo csubareas

--- src/main/java/mx/bidg/model/CSubareas.java
+++ src/main/java/mx/bidg/model/CSubareas.java
@@ -5,7 +5,9 @@
  */
 package mx.bidg.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import mx.bidg.config.JsonViews;
 import mx.bidg.utils.DateTimeConverter;
 import org.hibernate.annotations.DynamicUpdate;
@@ -25,6 +27,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Entity
 @DynamicUpdate
 @Table(name = "C_SUBAREAS")
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "_id")
 public class CSubareas implements Serializable {
 
     private static final long serialVersionUID = 1L;
