08098a501e52bf787387cf021eea5f642dfe928d
Julio Cesar <jsantiago@bidg.mx>
2017-05-19 15:15:22 -0500

    RequestPoducts
    se modifico los model

--- src/main/java/mx/bidg/model/RequestProducts.java
+++ src/main/java/mx/bidg/model/RequestProducts.java
@@ -9,15 +9,13 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import mx.bidg.config.JsonViews;
+import mx.bidg.utils.DateTimeConverter;
 import org.hibernate.annotations.DynamicUpdate;
 
 
@@ -28,7 +26,6 @@ import org.hibernate.annotations.DynamicUpdate;
 @Entity
 @DynamicUpdate
 @Table(name = "REQUEST_PRODUCTS")
-
 @JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "_id")
 public class RequestProducts implements Serializable {
     
@@ -44,38 +41,44 @@ public class RequestProducts implements Serializable {
     @JsonView(JsonViews.Root.class)
     private Integer idRequest;
     
-    @Column(name = "ID_PRODUCT", insertable = false, updatable = false)
+    @Column(name = "ID_ROLE_PRODUCT_REQUEST", insertable = false, updatable = false)
     @JsonView(JsonViews.Root.class)
-    private Integer idProduct;
+    private Integer idRoleProductRequest;
+
+    @Column(name = "QUANTITY", insertable = false, updatable = false)
+    @JsonView(JsonViews.Root.class)
+    private Integer quantity;
+
+    @Column(name = "ID_ACCESS_LEVEL")
+    @JsonView(JsonViews.Root.class)
+    private Integer idAccessLevel;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "CREATION_DATE")
+    @Convert(converter = DateTimeConverter.class)
+    @JsonView(JsonViews.Root.class)
+    private LocalDateTime creationDate;
+
+    @Size(max = 50)
+    @Column(name = "USERNAME")
+    @JsonView(JsonViews.Root.class)
+    private String username;
+
+    @JoinColumn(name = "ID_ROLE_PRODUCT_REQUEST", referencedColumnName = "ID_ROLE_PRODUCT_REQUEST")
+    @ManyToOne(optional = false)
+    @JsonView(JsonViews.Embedded.class)
+    private RoleProductRequest roleProductRequest;
     
     @JoinColumn(name = "ID_REQUEST", referencedColumnName = "ID_REQUEST")
     @ManyToOne(optional = false)
     @JsonView(JsonViews.Embedded.class)
     private Requests request;
-    
-    @JoinColumn(name = "ID_PRODUCT", referencedColumnName = "ID_PRODUCT")
-    @ManyToOne(optional = false)
-    @JsonView(JsonViews.Embedded.class)
-    private CProducts product;
-    
-    @Column(name = "ID_ACCESS_LEVEL")
-    @JsonView(JsonViews.Root.class)
-    private Integer idAccessLevel;
+
 
     public RequestProducts() {
     }
 
-    public RequestProducts(Integer idRequestProduct) {
-        this.idRequestProduct = idRequestProduct;
-    }
-
-    public Integer getIdRequestProduct() {
-        return idRequestProduct;
-    }
-
-    public void setIdRequestProduct(Integer idRequestProduct) {
-        this.idRequestProduct = idRequestProduct;
-    }
 
     public Integer getIdRequest() {
         return idRequest;
@@ -83,14 +86,6 @@ public class RequestProducts implements Serializable {
 
     public void setIdRequest(Integer idRequest) {
         this.idRequest = idRequest;
-    }
-
-    public Integer getIdProduct() {
-        return idProduct;
-    }
-
-    public void setIdProduct(Integer idProduct) {
-        this.idProduct = idProduct;
     }
 
     public Requests getRequest() {
@@ -101,12 +96,44 @@ public class RequestProducts implements Serializable {
         this.request = request;
     }
 
-    public CProducts getProduct() {
-        return product;
+    public Integer getIdRequestProduct() {
+        return idRequestProduct;
     }
 
-    public void setProduct(CProducts product) {
-        this.product = product;
+    public void setIdRequestProduct(Integer idRequestProduct) {
+        this.idRequestProduct = idRequestProduct;
+    }
+
+    public Integer getIdRoleProductRequest() {
+        return idRoleProductRequest;
+    }
+
+    public void setIdRoleProductRequest(Integer idRoleProductRequest) {
+        this.idRoleProductRequest = idRoleProductRequest;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public Integer getIdAccessLevel() {
@@ -117,29 +144,48 @@ public class RequestProducts implements Serializable {
         this.idAccessLevel = idAccessLevel;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (idRequestProduct != null ? idRequestProduct.hashCode() : 0);
-        return hash;
+    public RoleProductRequest getRoleProductRequest() {
+        return roleProductRequest;
+    }
+
+    public void setRoleProductRequest(RoleProductRequest roleProductRequest) {
+        this.roleProductRequest = roleProductRequest;
     }
 
     @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof RequestProducts)) {
-            return false;
-        }
-        RequestProducts other = (RequestProducts) object;
-        if ((this.idRequestProduct == null && other.idRequestProduct != null) || (this.idRequestProduct != null && !this.idRequestProduct.equals(other.idRequestProduct))) {
-            return false;
-        }
-        return true;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RequestProducts)) return false;
+
+        RequestProducts that = (RequestProducts) o;
+
+        if (!getIdRequestProduct().equals(that.getIdRequestProduct())) return false;
+        if (!getIdRequest().equals(that.getIdRequest())) return false;
+        if (!getIdRoleProductRequest().equals(that.getIdRoleProductRequest())) return false;
+        if (!getQuantity().equals(that.getQuantity())) return false;
+        if (!getIdAccessLevel().equals(that.getIdAccessLevel())) return false;
+        if (!getCreationDate().equals(that.getCreationDate())) return false;
+        if (!getUsername().equals(that.getUsername())) return false;
+        return getRequest().equals(that.getRequest());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getIdRequestProduct().hashCode();
+        result = 31 * result + getIdRequest().hashCode();
+        result = 31 * result + getIdRoleProductRequest().hashCode();
+        result = 31 * result + getQuantity().hashCode();
+        result = 31 * result + getIdAccessLevel().hashCode();
+        result = 31 * result + getCreationDate().hashCode();
+        result = 31 * result + getUsername().hashCode();
+        result = 31 * result + getRequest().hashCode();
+        return result;
     }
 
     @Override
     public String toString() {
         return "mx.bidg.model.RequestProducts[ idRequestProduct=" + idRequestProduct + " ]";
     }
-    
+
+
 }
