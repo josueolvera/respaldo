15c6562fd6cd08da05106cb90dbde0b2265a6cf8
josueolverar <josue.olvera.r@gmail.com>
2017-06-16 08:03:18 -0500

    Se modifica Controller RequestOrderDocumentsController para que funcione el guardado correctamente y se agregan otros controladores

--- src/main/java/mx/bidg/controller/siad/RequestOrderDocumentsController.java
+++ src/main/java/mx/bidg/controller/siad/RequestOrderDocumentsController.java
@@ -3,20 +3,21 @@ package mx.bidg.controller.siad;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import mx.bidg.config.JsonViews;
-import mx.bidg.model.RequestOrderDocuments;
-import mx.bidg.model.Users;
-import mx.bidg.service.RequestOrderDocumentsService;
+import mx.bidg.model.*;
+import mx.bidg.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -33,6 +34,21 @@ public class RequestOrderDocumentsController {
     @Autowired
     private ObjectMapper mapper;
 
+    @Autowired
+    private RequestOrderDetailService requestOrderDetailService;
+
+    @Autowired
+    private RequestsService requestsService;
+
+    @Autowired
+    private PriceEstimationsService priceEstimationsService;
+
+    @Autowired
+    private ProvidersService providersService;
+
+    @Autowired
+    private CDistributorsService cDistributorsService;
+
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<String> findAll() throws IOException {
         List<RequestOrderDocuments> accountingAccounts = requestOrderDocumentsService.findAll();
@@ -43,18 +59,84 @@ public class RequestOrderDocumentsController {
     @RequestMapping(value = "/save-order-documents",
             method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<String> save(@RequestBody String data, HttpSession session) throws IOException {
-        int pediente = 10;
+
         JsonNode node = mapper.readTree(data);
+
         Users user = (Users) session.getAttribute("user");
+
+        Requests request = requestsService.findById(node.get("idRequest").asInt());
+
         RequestOrderDocuments requestOrderDocuments = new RequestOrderDocuments();
-        requestOrderDocuments.setIdPriceEstimation(pediente);
-        requestOrderDocuments.setFilePath(node.get("filePath").asText());
-        requestOrderDocuments.setFileName(node.get("fileName").asText());
-        requestOrderDocuments.setCreationDate(LocalDateTime.now());
-        requestOrderDocuments.setUsername(user.getUsername());
-        requestOrderDocumentsService.save(requestOrderDocuments);
+
+        if (request != null){
+
+            requestOrderDocuments.setRequest(request);
+
+            Providers provider = providersService.findById(node.get("provider").get("idProvider").asInt());
+            if (provider != null){
+                requestOrderDocuments.setProvider(provider);
+            }
+
+            PriceEstimations priceEstimations = priceEstimationsService.findAuthorized(request.getIdRequest());
+            if (priceEstimations != null){
+                requestOrderDocuments.setPriceEstimations(priceEstimations);
+            }
+
+            CDistributors distributor = cDistributorsService.findById(node.get("distributor").get("idDistributor").asInt());
+            if (distributor != null){
+                requestOrderDocuments.setcDistributor(distributor);
+            }
+
+            requestOrderDocuments.setContactNameTransmitter(node.get("contactName").asText());
+            requestOrderDocuments.setContactTelTransmitter(node.get("contactCel").asText());
+            requestOrderDocuments.setContactRoleTransmitter(node.get("contactRole").asText());
+            requestOrderDocuments.setAmount(new BigDecimal(node.get("amount").asDouble()));
+            requestOrderDocuments.setIva(new BigDecimal(node.get("iva").asDouble()));
+            requestOrderDocuments.setTotalAmount(new BigDecimal(node.get("totalAmount").asDouble()));
+            requestOrderDocuments.setCreationDate(LocalDateTime.now());
+            requestOrderDocuments.setUsername(user.getUsername());
+
+
+            requestOrderDocuments = requestOrderDocumentsService.save(requestOrderDocuments);
+
+            for (JsonNode jNode : node.get("orderProducts")){
+                RequestOrderDetail requestOrderDetail = new RequestOrderDetail();
+                requestOrderDetail.setRequestOrderDocuments(requestOrderDocuments);
+                requestOrderDetail.setUsername(user.getUsername());
+                requestOrderDetail.setCreationDate(LocalDateTime.now());
+                requestOrderDetail.setDescription(jNode.get("description").asText());
+                requestOrderDetail.setQuantity(jNode.get("quantity").asInt());
+                requestOrderDetail.setPrice(new BigDecimal(jNode.get("price").asText()));
+
+                requestOrderDetailService.save(requestOrderDetail);
+            }
+        }
 
         return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString
-                (requestOrderDocumentsService.findAll()), HttpStatus.OK);
+                (requestOrderDocuments), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/request/{idRequest}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> findByIdRequest(@PathVariable Integer idRequest)throws Exception{
+        RequestOrderDocuments requestOrderDocument = requestOrderDocumentsService.findByIdRequest(idRequest);
+
+        if (requestOrderDocument != null){
+            List<RequestOrderDetail> requestOrderDetails = requestOrderDetailService.findByidReqOrderDocument(requestOrderDocument.getIdRequestOrderDocument());
+
+            if (!requestOrderDetails.isEmpty()){
+                requestOrderDocument.setRequestOrderDetails(requestOrderDetails);
+            }
+        }
+
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(requestOrderDocument), HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/{idRequestOrderDocument}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> delete(@PathVariable Integer idRequestOrderDocument)throws Exception{
+
+        RequestOrderDocuments requestOrderDocuments = requestOrderDocumentsService.findById(idRequestOrderDocument);
+        boolean status = requestOrderDocumentsService.delete(requestOrderDocuments);
+
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(status), HttpStatus.OK);
     }
 }
