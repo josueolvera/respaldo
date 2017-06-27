ff5f7202ef524191ae4a155db5415b2053b30d2c
Josue Olvera <josue.olvera.r@gmail.com>
2017-06-26 20:04:06 -0500

    Se agrega controller de obtencion de distribuidorAreaRol por usuario

--- src/main/java/mx/bidg/controller/DistributorAreaRolController.java
+++ src/main/java/mx/bidg/controller/DistributorAreaRolController.java
@@ -3,6 +3,7 @@ package mx.bidg.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import mx.bidg.config.JsonViews;
 import mx.bidg.model.DistributorAreaRol;
+import mx.bidg.model.Users;
 import mx.bidg.service.DistributorAreaRolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -39,5 +41,13 @@ public class DistributorAreaRolController {
     public ResponseEntity<String> getRolByArea(@PathVariable Integer idArea) throws IOException{
         List<DistributorAreaRol> distributorAreaRols = distributorAreaRolService.findRolByArea(idArea);
         return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(distributorAreaRols),HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/by-user", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> getDistributorAreaRolByUser(HttpSession session) throws IOException{
+        Users user = (Users) session.getAttribute("user");
+        DistributorAreaRol distributorAreaRol = distributorAreaRolService.findByCombination(user.getDwEmployee().getDwEnterprise().getDistributor().getIdDistributor(), user.getDwEmployee().getDwEnterprise().getArea().getIdArea(), user.getDwEmployee().getRole().getIdRole());
+
+        return new ResponseEntity<>(mapper.writerWithView(JsonViews.Embedded.class).writeValueAsString(distributorAreaRol), HttpStatus.OK);
     }
 }
