f6d03e9d61c9df18fbec75477b35e4f9fd6f3470
Josue Olvera <josue.olvera.r@gmail.com>
2017-06-26 19:53:16 -0500

    Se agrega busqeda por idDistributorCostCenter, mes y año para obtener el total presupuestado y el total gastado

--- src/main/java/mx/bidg/dao/RequestBudgetSpendingDao.java
+++ src/main/java/mx/bidg/dao/RequestBudgetSpendingDao.java
@@ -2,9 +2,13 @@ package mx.bidg.dao;
 
 import mx.bidg.model.RequestBudgetSpending;
 
+import java.math.BigDecimal;
+
 /**
  * Created by desarrollador on 19/06/17.
  */
 public interface RequestBudgetSpendingDao extends InterfaceDao<RequestBudgetSpending> {
     RequestBudgetSpending findByIdDistributorCostCenterYear(Integer idDistributorCostCenter, int year);
+    BigDecimal getAmountDistributorCostCenter(Integer idDistributorCostCenter, int month, int year);
+    BigDecimal getAmountExpendedDistributorCostCenter(Integer idDistributorCostCenter, int month, int year);
 }
--- src/main/java/mx/bidg/dao/impl/RequestBudgetSpendingDaoImpl.java
+++ src/main/java/mx/bidg/dao/impl/RequestBudgetSpendingDaoImpl.java
@@ -3,9 +3,13 @@ package mx.bidg.dao.impl;
 import mx.bidg.dao.AbstractDao;
 import mx.bidg.dao.RequestBudgetSpendingDao;
 import mx.bidg.model.RequestBudgetSpending;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Projection;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -45,6 +49,104 @@ public class RequestBudgetSpendingDaoImpl extends AbstractDao<Integer, RequestBu
     @Override
     public RequestBudgetSpending findByIdDistributorCostCenterYear(Integer idDistributorCostCenter, int year) {
         return (RequestBudgetSpending) createEntityCriteria()
+                .add(Restrictions.eq("idDistributorCostCenter", idDistributorCostCenter))
+                .add(Restrictions.eq("year", year))
+                .uniqueResult();
+    }
+
+    @Override
+    public BigDecimal getAmountDistributorCostCenter(Integer idDistributorCostCenter, int month, int year) {
+        Criteria criteria = createEntityCriteria();
+
+        switch (month){
+            case 1:
+                criteria.setProjection(Projections.sum("januaryAmount"));
+                break;
+            case 2:
+                criteria.setProjection(Projections.sum("februaryAmount"));
+                break;
+            case 3:
+                criteria.setProjection(Projections.sum("marchAmount"));
+                break;
+            case 4:
+                criteria.setProjection(Projections.sum("aprilAmount"));
+                break;
+            case 5:
+                criteria.setProjection(Projections.sum("mayAmount"));
+                break;
+            case 6:
+                criteria.setProjection(Projections.sum("juneAmount"));
+                break;
+            case 7:
+                criteria.setProjection(Projections.sum("julyAmount"));
+                break;
+            case 8:
+                criteria.setProjection(Projections.sum("augustAmount"));
+                break;
+            case 9:
+                criteria.setProjection(Projections.sum("septemberAmount"));
+                break;
+            case 10:
+                criteria.setProjection(Projections.sum("octoberAmount"));
+                break;
+            case 11:
+                criteria.setProjection(Projections.sum("novemberAmount"));
+                break;
+            case 12:
+                criteria.setProjection(Projections.sum("decemberAmount"));
+                break;
+
+        }
+        return (BigDecimal) criteria
+                .add(Restrictions.eq("idDistributorCostCenter", idDistributorCostCenter))
+                .add(Restrictions.eq("year", year))
+                .uniqueResult();
+    }
+
+    @Override
+    public BigDecimal getAmountExpendedDistributorCostCenter(Integer idDistributorCostCenter, int month, int year) {
+        Criteria criteria = createEntityCriteria();
+
+        switch (month){
+            case 1:
+                criteria.setProjection(Projections.sum("januarySpended"));
+                break;
+            case 2:
+                criteria.setProjection(Projections.sum("februarySpended"));
+                break;
+            case 3:
+                criteria.setProjection(Projections.sum("marchSpended"));
+                break;
+            case 4:
+                criteria.setProjection(Projections.sum("aprilSpended"));
+                break;
+            case 5:
+                criteria.setProjection(Projections.sum("maySpended"));
+                break;
+            case 6:
+                criteria.setProjection(Projections.sum("juneSpended"));
+                break;
+            case 7:
+                criteria.setProjection(Projections.sum("julySpended"));
+                break;
+            case 8:
+                criteria.setProjection(Projections.sum("augustSpended"));
+                break;
+            case 9:
+                criteria.setProjection(Projections.sum("septemberSpended"));
+                break;
+            case 10:
+                criteria.setProjection(Projections.sum("octoberSpended"));
+                break;
+            case 11:
+                criteria.setProjection(Projections.sum("novemberSpended"));
+                break;
+            case 12:
+                criteria.setProjection(Projections.sum("decemberSpended"));
+                break;
+
+        }
+        return (BigDecimal) criteria
                 .add(Restrictions.eq("idDistributorCostCenter", idDistributorCostCenter))
                 .add(Restrictions.eq("year", year))
                 .uniqueResult();
